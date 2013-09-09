2965f593a707cd156fa818122cb2ff4babbb5f0c
michaeldfallen <michaeldfallen@gmail.com>
2013-09-09 14:02:34 +0100

    fixed up the update-template script

--- update-template.sh
+++ update-template.sh
@@ -1,5 +1,22 @@
 thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+
+function removeSubmodule() {
+  submodule="$(cat $thisDir/.gitmodules | grep "path = .*$1" | grep -Eo "\S*$1")"
+  if [[ -n $submodule ]]; then 
+    rm -rf "$thisDir/$submodule"
+    rm -rf "$thisDir/.gitmodules"
+    rm -rf "$thisDir/.git/modules/$1"
+    git rm "$thisDir/$submodule"
+  fi
+}
+
+
+if [ -z "$(cat .gitmodules | grep "https://github.com/alphagov/govuk_template_play.git")" ]; then
+  removeSubmodule govuk_template_play
+  git submodule add https://github.com/alphagov/govuk_template_play.git  
+fi
+
 cd "$thisDir"
 echo "Updating govuk_template_play"
 git submodule init
