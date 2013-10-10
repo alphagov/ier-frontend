15c195356643f2c6c758bc4ab93e8f8d4e94bf1c
Peter Souter <p.morsou@gmail.com>
2013-10-10 17:08:06 +0100

    Fixed issue with Sass gem not in path

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,5 +1,9 @@
 echo "Build IER for Jenkins"
 
+echo "Install Sass Gem"
+bundle install --quiet
+echo "Sass Gem Install"
+
 echo "Clean and update the build"
 ./scripts/play/play "clean-files"
 ./scripts/play/play "update"
