96d9e7fedfd6bc8fdda4a7ec0386cfa26bb8c16e
Antonio Perez <a.perez@kainos.com>
2013-12-06 15:11:20 +0000

    configured logback

--- app/uk/gov/gds/ier/DynamicGlobal.scala
+++ app/uk/gov/gds/ier/DynamicGlobal.scala
@@ -1,14 +1,13 @@
 package uk.gov.gds.ier
 
 import play.api.{Application, GlobalSettings}
-import java.lang.Class
-import uk.gov.gds.ier.client.ApiClient
 import uk.gov.gds.guice.GuiceContainer
 import com.google.inject.{Binder, AbstractModule}
 import uk.gov.gds.ier.logging.Logging
 import uk.gov.gds.ier.config.Config
 import play.api.mvc.{SimpleResult, Handler, RequestHeader}
 import scala.concurrent.Future
+import org.slf4j.MDC
 
 trait DynamicGlobal extends GlobalSettings with Logging {
 
@@ -27,11 +26,13 @@ trait DynamicGlobal extends GlobalSettings with Logging {
 
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
     logger.debug(s"routing request ${request.method} ${request.path}")
-    super.onRouteRequest(request)
+    MDC.put("clientip", request.headers.get("X-Real-IP").getOrElse("N/A"))
+    val handler = super.onRouteRequest(request)
+    handler
   }
 
   override def onError(request: RequestHeader, ex: Throwable): Future[SimpleResult] = {
-    logger.error(s"uncaught exception when routing request ${request.method} ${request.path} exception: ${ex.getStackTraceString}")
+    logger.error(s"uncaught exception when routing request ${request.method} ${request.path}", ex)
     super.onError(request, ex)
   }
 }
