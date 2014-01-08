26a977f99780d4afdb7f267e84c36522be7e4e62
Antonio Perez <a.perez@kainos.com>
2014-01-08 17:31:22 +0000

    minor changes

--- app/uk/gov/gds/ier/controller/ErrorController.scala
+++ app/uk/gov/gds/ier/controller/ErrorController.scala
@@ -2,12 +2,15 @@ package uk.gov.gds.ier.controller
 
 import play.api.mvc._
 import views._
+import uk.gov.gds.ier.config.Config
+import com.google.inject.Inject
+import uk.gov.gds.ier.guice.WithConfig
 
-class ErrorController
-  extends Controller {
+class ErrorController @Inject ()(val config: Config)
+  extends Controller
+    with WithConfig {
 
   def timeout = Action {
-    Ok(html.error.timeoutError())
+    Ok(html.error.timeoutError(config.sessionTimeout))
   }
 }
-
