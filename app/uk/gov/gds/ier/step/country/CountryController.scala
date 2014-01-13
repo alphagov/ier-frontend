a08f2c495709c0843e73541c7dbb7d32a1272c6e
michaeldfallen <michaeldfallen@gmail.com>
2014-01-13 11:00:03 +0000

    Revert "Merge pull request #103 from gds/ma_revertCountryMustache"
    This reverts commit 7e851256e56a65a01b6a9d8f68a1e6bbc637c262, reversing
    changes made to e6ac72c077446acaf5b06c2a8f3eb8fa9fdffbdb.

--- app/uk/gov/gds/ier/step/country/CountryController.scala
+++ app/uk/gov/gds/ier/step/country/CountryController.scala
@@ -12,11 +12,14 @@ import play.api.templates.Html
 import uk.gov.gds.ier.guice.{WithEncryption, WithConfig}
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.security.{EncryptionKeys, EncryptionService}
+import org.jba.Mustache
+import views.html.layouts.{stepsBodyEnd, head}
 
 class CountryController @Inject ()(val serialiser: JsonSerialiser,
                                    val config:Config,
                                    val encryptionService : EncryptionService,
-                                   val encryptionKeys : EncryptionKeys)
+                                   val encryptionKeys : EncryptionKeys,
+                                   val countryTransformer : CountryMustacheTransformer)
   extends StepController
   with CountryConstraints
   with WithSerialiser
@@ -29,8 +32,11 @@ class CountryController @Inject ()(val serialiser: JsonSerialiser,
   val stepPostRoute = step.routes.CountryController.post
 
   def template(form:InProgressForm, call:Call): Html = {
-    views.html.steps.country(form, call)
+    val data = countryTransformer.transformFormStepToMustacheData(form,call.url).getOrElse(None)
+    val content:Html = Mustache.render("ordinary/country", data)
+    views.html.layouts.main (title = Some("Register to Vote - Where do you live?"),stylesheets = head(), scripts = stepsBodyEnd())(content)
   }
+
   def goToNext(currentState: InprogressApplication): SimpleResult = {
     currentState.country match {
       case Some(Country("Northern Ireland")) => Redirect(routes.ExitController.northernIreland)
