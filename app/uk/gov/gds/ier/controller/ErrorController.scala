e2dedfdcc3a3e3a51eba7314105d3d1c44efc219
Antonio Perez <a.perez@kainos.com>
2014-01-15 14:41:11 +0000

    changes because of merge

--- app/uk/gov/gds/ier/controller/ErrorController.scala
+++ app/uk/gov/gds/ier/controller/ErrorController.scala
@@ -1,16 +1,27 @@
 package uk.gov.gds.ier.controller
 
 import play.api.mvc._
-import views._
 import uk.gov.gds.ier.config.Config
 import com.google.inject.Inject
-import uk.gov.gds.ier.guice.WithConfig
+import uk.gov.gds.ier.guice.{WithEncryption, WithConfig}
+import uk.gov.gds.ier.serialiser.{WithSerialiser, JsonSerialiser}
+import uk.gov.gds.ier.security.{EncryptionKeys, EncryptionService}
+import uk.gov.gds.ier.session.SessionCleaner
+import uk.gov.gds.ier.logging.Logging
 
-class ErrorController @Inject ()(val config: Config)
+class ErrorController @Inject ()(val serialiser: JsonSerialiser,
+                                 val config: Config,
+                                 val encryptionService : EncryptionService,
+                                 val encryptionKeys : EncryptionKeys)
   extends Controller
-    with WithConfig {
+  with SessionCleaner
+  with WithSerialiser
+  with Logging
+  with WithConfig
+  with WithEncryption {
 
-  def timeout = Action {
-    Ok(html.error.timeoutError(config.sessionTimeout))
+  def timeout = ClearSession requiredFor {
+    request =>
+      Ok(views.html.error.timeoutError(config.sessionTimeout))
   }
 }
