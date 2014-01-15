91a0c3a0ca85c26fcbe84206845a6d19e5385d87
Antonio Perez <a.perez@kainos.com>
2014-01-15 12:38:38 +0000

    implementation of the story (2)

---
+++ app/uk/gov/gds/ier/validation/constraints/PostalVoteConstraints.scala
@@ -0,0 +1,28 @@
+package uk.gov.gds.ier.validation.constraints
+
+import uk.gov.gds.ier.validation.{FormKeys, ErrorMessages, NinoValidator}
+import play.api.data.validation.{Valid, Invalid, Constraint}
+import uk.gov.gds.ier.model._
+
+trait PostalVoteConstraints {
+  self: ErrorMessages
+    with FormKeys =>
+
+  lazy val validDeliveryMethod = Constraint[PostalVoteDeliveryMethod](keys.deliveryMethod.key) {
+    postaVoteDeliveryMethod =>
+      if (postaVoteDeliveryMethod.deliveryMethod.isDefined)
+        if (postaVoteDeliveryMethod.deliveryMethod.equals("email") && !postaVoteDeliveryMethod.emailAddress.isDefined )
+          Invalid("Please enter the email address", keys.deliveryMethod.emailAddress)
+        else Valid
+      else Invalid("Please answer this question", keys.deliveryMethod)
+  }
+
+  lazy val validPostVoteOption = Constraint[InprogressOrdinary](keys.deliveryMethod.key) {
+    application => {
+      if (application.postalVoteOptin.isDefined && application.postalVoteOptin.get == true && !application.postalVoteDeliveryMethod.isDefined)
+        Invalid("Please enter your email address", keys.deliveryMethod)
+      else
+        Valid
+    }
+  }
+}
