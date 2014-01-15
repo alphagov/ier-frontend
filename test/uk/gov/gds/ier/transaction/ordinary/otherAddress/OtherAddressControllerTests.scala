d8819ec1f38f2a1dd7da6b7b83e02f3d4c9955dc
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-01-15 16:59:24 +0000

    Revert to original test as reasons for switching to Specs2 exist no more. Add trivial fix as html entity is not used in Mustachified page.

--- test/uk/gov/gds/ier/transaction/ordinary/otherAddress/OtherAddressControllerTests.scala
+++ test/uk/gov/gds/ier/transaction/ordinary/otherAddress/OtherAddressControllerTests.scala
@@ -1,108 +1,104 @@
 package uk.gov.gds.ier.transaction.ordinary.otherAddress
 
-import org.specs2.mutable._
-import org.specs2.runner.JUnitRunner
-import org.junit.runner._
+import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.mock.MockitoSugar
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import play.api.test._
 import play.api.test.Helpers._
-import play.api.test.{FakeRequest, FakeApplication}
-import org.specs2.matcher.Matchers
 import uk.gov.gds.ier.test.TestHelpers
 
-// Temporarily block runner due to SBT issue: https://github.com/sbt/junit-interface/issues/30
-// @RunWith(classOf[JUnitRunner])
 class OtherAddressControllerTests
-  extends Specification
+  extends FlatSpec
   with Matchers
+  with MockitoSugar
   with TestHelpers {
-  "OtherAddressController.get" should {
-    "display the page" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(GET, "/register-to-vote/other-address").withIerSession()
-        )
 
-        status(result) must be_==(OK)
-        contentType(result) must be_==(Some("text/html"))
-        contentAsString(result) must contain("Question 8")
-        contentAsString(result) must contain("<a class=\"back-to-previous\" href=\"/register-to-vote/previous-address")
-        contentAsString(result) must contain("Do you live at a second UK address where you're registered to vote?")
-        contentAsString(result) must contain("/register-to-vote/other-address")
-      }
+  behavior of "OtherAddressController.get"
+  it should "display the page" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(GET, "/register-to-vote/other-address").withIerSession()
+      )
+
+      status(result) should be(OK)
+      contentType(result) should be(Some("text/html"))
+      contentAsString(result) should include("Question 8")
+      contentAsString(result) should include("<a class=\"back-to-previous\" href=\"/register-to-vote/previous-address")
+      contentAsString(result) should include("Do you live at a second UK address where you're registered to vote?")
+      contentAsString(result) should include("/register-to-vote/other-address")
     }
   }
 
-  "OtherAddressController.post" should {
-    "bind successfully and redirect to the Open Register step" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(POST, "/register-to-vote/other-address")
-            .withIerSession()
-            .withFormUrlEncodedBody(
-              "otherAddress.hasOtherAddress" -> "true"
-            )
-        )
+  behavior of "OtherAddressController.post"
+  it should "bind successfully and redirect to the Open Register step" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(POST, "/register-to-vote/other-address")
+          .withIerSession()
+          .withFormUrlEncodedBody(
+            "otherAddress.hasOtherAddress" -> "true"
+          )
+      )
 
-        status(result) must be_==(SEE_OTHER)
-        redirectLocation(result) must be_==(Some("/register-to-vote/open-register"))
-      }
-    }
-
-    "display any errors on unsuccessful bind" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(POST, "/register-to-vote/other-address").withIerSession()
-        )
-
-        status(result) must be_==(OK)
-        contentAsString(result) must contain("Do you live at a second UK address where you're registered to vote?")
-        contentAsString(result) must contain("Please answer this question")
-        contentAsString(result) must contain("/register-to-vote/other-address")
-      }
+      status(result) should be(SEE_OTHER)
+      redirectLocation(result) should be(Some("/register-to-vote/open-register"))
     }
   }
 
-  "OtherAddressController.editGet" should {
-    "display the edit page" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(GET, "/register-to-vote/edit/other-address").withIerSession()
-        )
+  it should "display any errors on unsuccessful bind" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(POST, "/register-to-vote/other-address").withIerSession()
+      )
 
-        status(result) must be_==(OK)
-        contentType(result) must be_==(Some("text/html"))
-        contentAsString(result) must contain("Do you live at a second UK address where you're registered to vote?")
-        contentAsString(result) must contain("/register-to-vote/edit/other-address")
-      }
+      status(result) should be(OK)
+      contentAsString(result) should include("Do you live at a second UK address where you're registered to vote?")
+      contentAsString(result) should include("Please answer this question")
+      contentAsString(result) should include("/register-to-vote/other-address")
     }
   }
 
-  "OtherAddressController.editPost" should {
-    "bind successfully and redirect to the Confirmation step" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(POST, "/register-to-vote/edit/other-address")
-            .withIerSession()
-            .withFormUrlEncodedBody(
-              "otherAddress.hasOtherAddress" -> "true"
-            )
-        )
+  behavior of "OtherAddressController.editGet"
+  it should "display the edit page" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(GET, "/register-to-vote/edit/other-address").withIerSession()
+      )
 
-        status(result) must be_==(SEE_OTHER)
-        redirectLocation(result) must be_==(Some("/register-to-vote/confirmation"))
-      }
+      status(result) should be(OK)
+      contentType(result) should be(Some("text/html"))
+      contentAsString(result) should include("Do you live at a second UK address where you're registered to vote?")
+      contentAsString(result) should include("/register-to-vote/edit/other-address")
     }
+  }
 
-    "display any errors on unsuccessful bind" in {
-      running(FakeApplication()) {
-        val Some(result) = route(
-          FakeRequest(POST, "/register-to-vote/edit/other-address").withIerSession()
-        )
+  behavior of "OtherAddressController.editPost"
+  it should "bind successfully and redirect to the Confirmation step" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(POST, "/register-to-vote/edit/other-address")
+          .withIerSession()
+          .withFormUrlEncodedBody(
+            "otherAddress.hasOtherAddress" -> "true"
+          )
+      )
 
-        status(result) must be_==(OK)
-        contentAsString(result) must contain("Do you live at a second UK address where you're registered to vote?")
-        contentAsString(result) must contain("Please answer this question")
-        contentAsString(result) must contain("/register-to-vote/edit/other-address")
-      }
+      status(result) should be(SEE_OTHER)
+      redirectLocation(result) should be(Some("/register-to-vote/confirmation"))
+    }
+  }
+
+  it should "display any errors on unsuccessful bind" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(POST, "/register-to-vote/edit/other-address").withIerSession()
+      )
+
+      status(result) should be(OK)
+      contentAsString(result) should include("Do you live at a second UK address where you're registered to vote?")
+      contentAsString(result) should include("Please answer this question")
+      contentAsString(result) should include("/register-to-vote/edit/other-address")
     }
   }
 }
