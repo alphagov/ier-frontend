2809418f0c02f78c7612ea021af28a5427b62b41
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-01-20 15:08:08 +0000

    Improve tests. Add tests to check what values are delivered to the rendered page through Mustache model.

--- test/uk/gov/gds/ier/transaction/ordinary/name/NameControllerTests.scala
+++ test/uk/gov/gds/ier/transaction/ordinary/name/NameControllerTests.scala
@@ -2,11 +2,10 @@ package uk.gov.gds.ier.transaction.ordinary.name
 
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.mock.MockitoSugar
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
 import play.api.test._
 import play.api.test.Helpers._
 import uk.gov.gds.ier.test.TestHelpers
+import uk.gov.gds.ier.model.{PreviousName, Name, InprogressOrdinary}
 
 class NameControllerTests
   extends FlatSpec
@@ -200,6 +199,141 @@ class NameControllerTests
       contentAsString(result) should include("Have you changed your name in the last 12 months?")
       contentAsString(result) should include("Please answer this question")
       contentAsString(result) should include("/register-to-vote/edit/name")
+    }
+  }
+
+  behavior of "access 'name page' with name already filled and no previous name filled"
+  it should "display the page with name filled and previous name radio button set to false" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(GET, "/register-to-vote/name")
+          .withIerSession(3)
+          .withApplication(InprogressOrdinary(
+          name = Some(Name(
+            firstName = "John",
+            middleNames = Some("Archibald William"),
+            lastName= "Smith"
+          ))))
+      )
+
+      status(result) should be(OK)
+      contentType(result) should be(Some("text/html"))
+      contentAsString(result) should include("Question 4")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_firstName\" name=\"name.firstName\" value=\"John\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_middleNames\" name=\"name.middleNames\" value=\"Archibald William\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_lastName\" name=\"name.lastName\" value=\"Smith\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_true\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"true\" class=\"radio validate\" data-validation-name=\"previousYes\" data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" >")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_false\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"false\"  class=\"radio  validate\"   data-validation-name=\"previousNo\"  data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" >")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_firstName\" name=\"previousName.previousName.firstName\" value=\"\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_middleNames\" name=\"previousName.previousName.middleNames\" value=\"\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_lastName\" name=\"previousName.previousName.lastName\" value=\"\"")
+    }
+  }
+
+  behavior of "access 'name page' with name already filled and also previous name filled"
+  it should "display the page with name printed and previous name radio button set to true and previous name printed" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(GET, "/register-to-vote/name")
+          .withIerSession(3)
+          .withApplication(InprogressOrdinary(
+          name = Some(Name(
+            firstName = "John",
+            middleNames = Some("Archibald William"),
+            lastName= "Smith"
+          )),
+          previousName = Some(PreviousName(
+            hasPreviousName = true,
+            previousName = Some(Name(
+              firstName = "Jan",
+              middleNames = Some("Johannes"),
+              lastName= "Kovar"
+            ))
+          ))
+        ))
+      )
+
+      status(result) should be(OK)
+      contentType(result) should be(Some("text/html"))
+      contentAsString(result) should include("Question 4")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_firstName\" name=\"name.firstName\" value=\"John\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_middleNames\" name=\"name.middleNames\" value=\"Archibald William\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"name_lastName\" name=\"name.lastName\" value=\"Smith\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_true\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"true\" class=\"radio validate\" data-validation-name=\"previousYes\" data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" checked>")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_false\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"false\"  class=\"radio  validate\"   data-validation-name=\"previousNo\"  data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" >")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_firstName\" name=\"previousName.previousName.firstName\" value=\"Jan\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_middleNames\" name=\"previousName.previousName.middleNames\" value=\"Johannes\"")
+
+      contentAsString(result) should include("" +
+        "<input type=\"text\" id=\"previousName_previousName_lastName\" name=\"previousName.previousName.lastName\" value=\"Kovar\"")
+    }
+  }
+
+  behavior of "access 'name page' with previous name is set explicitly to false"
+  it should "display the page where previous name radio button is explicitly set to false" in {
+    running(FakeApplication()) {
+      val Some(result) = route(
+        FakeRequest(GET, "/register-to-vote/name")
+          .withIerSession(3)
+          .withApplication(InprogressOrdinary(
+            previousName = Some(PreviousName(
+              hasPreviousName = false,
+              previousName = None
+            ))
+        ))
+      )
+
+      status(result) should be(OK)
+      contentType(result) should be(Some("text/html"))
+      contentAsString(result) should include("Question 4")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_true\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"true\" class=\"radio validate\" data-validation-name=\"previousYes\" data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" >")
+
+      contentAsString(result) should include("" +
+        "<input type=\"radio\" id=\"previousName_hasPreviousName_false\" name=\"previousName.hasPreviousName\"\n" +
+        "                   value=\"false\"  class=\"radio  validate\"   data-validation-name=\"previousNo\"  data-validation-type=\"field\"\n" +
+        "                   data-validation-rules=\"nonEmpty\" checked>")
     }
   }
 }
