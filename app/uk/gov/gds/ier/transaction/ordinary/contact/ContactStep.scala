dc9f7eb3b5ea705cac5300817906c4565f68d9b8
Antonio Perez <a.perez@kainos.com>
2014-01-20 14:54:52 +0000

    overriding GET method to prepopulate email field (in case it is empty)

--- app/uk/gov/gds/ier/transaction/ordinary/contact/ContactStep.scala
+++ app/uk/gov/gds/ier/transaction/ordinary/contact/ContactStep.scala
@@ -6,13 +6,20 @@ import com.google.inject.Inject
 import uk.gov.gds.ier.serialiser.{WithSerialiser, JsonSerialiser}
 import uk.gov.gds.ier.validation._
 import play.api.mvc.{SimpleResult, Call}
-import uk.gov.gds.ier.model.{InprogressOrdinary, InprogressApplication}
+import uk.gov.gds.ier.model._
 import play.api.templates.Html
 
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.guice.{WithEncryption, WithConfig}
 import uk.gov.gds.ier.security.{EncryptionKeys, EncryptionService}
 import uk.gov.gds.ier.step.{OrdinaryStep, Routes}
+import play.api.mvc.Call
+import uk.gov.gds.ier.step.Routes
+import uk.gov.gds.ier.model.PostalVote
+import uk.gov.gds.ier.model.PostalVoteDeliveryMethod
+import uk.gov.gds.ier.model.InprogressOrdinary
+import uk.gov.gds.ier.validation.InProgressForm
+import scala.Some
 
 class ContactStep @Inject ()(val serialiser: JsonSerialiser,
                              val config: Config,
@@ -30,6 +37,49 @@ class ContactStep @Inject ()(val serialiser: JsonSerialiser,
     editGet = ContactController.editGet,
     editPost = ContactController.editPost
   )
+
+  override def get(implicit manifest: Manifest[InprogressOrdinary]) = ValidSession requiredFor {
+    request => application =>
+      logger.debug(s"GET request for ${request.path}")
+      Ok(template(InProgressForm(validation.fill(prepopulateEmailAddress(application))), routes.post, previousRoute))
+  }
+
+  def prepopulateEmailAddress (application:InprogressOrdinary):InprogressOrdinary = {
+    application.postalVote match {
+      case Some(PostalVote(_,Some(PostalVoteDeliveryMethod(_,Some(emailAddress))))) => {
+        application.contact match {
+          case Some(Contact(_,_,Some(_))) => {
+            val updatedApplication = application.copy(
+              contact = Some (application.contact.get.copy (
+                  email = Some (application.contact.get.email.get.copy(
+                        detail = Some(emailAddress)
+                     )
+                  )
+                )
+              )
+            )
+            updatedApplication
+          }
+          case Some(Contact(_,_,None)) => {
+            val updatedApplication = application.copy(
+              contact = Some (application.contact.get.copy (
+                  email = Some(ContactDetail(false,Some(emailAddress)))
+                )
+              )
+            )
+            updatedApplication
+          }
+          case None => {
+            val updatedApplication = application.copy(
+              contact = Some (Contact(false, None, Some(ContactDetail(false,Some(emailAddress)))))
+            )
+            updatedApplication
+          }
+        }
+      }
+      case _ => application
+    }
+  }
 
   def template(form:InProgressForm[InprogressOrdinary], call:Call, backUrl: Option[Call]): Html = {
     views.html.steps.contact(form, call, backUrl.map(_.url))
--- app/uk/gov/gds/ier/transaction/ordinary/postalVote/PostalVoteStep.scala
+++ app/uk/gov/gds/ier/transaction/ordinary/postalVote/PostalVoteStep.scala
@@ -3,16 +3,19 @@ package uk.gov.gds.ier.transaction.ordinary.postalVote
 import controllers.step.ordinary.ContactController
 import controllers.step.ordinary.routes.{PostalVoteController, OpenRegisterController}
 import com.google.inject.Inject
-import uk.gov.gds.ier.serialiser.{WithSerialiser, JsonSerialiser}
-import uk.gov.gds.ier.validation._
-import play.api.mvc.{SimpleResult, Call}
-import uk.gov.gds.ier.model.{Contact, InprogressOrdinary, InprogressApplication}
+import uk.gov.gds.ier.serialiser.JsonSerialiser
+import uk.gov.gds.ier.model._
 import play.api.templates.Html
 
 import uk.gov.gds.ier.config.Config
-import uk.gov.gds.ier.guice.{WithEncryption, WithConfig}
 import uk.gov.gds.ier.security.{EncryptionKeys, EncryptionService}
-import uk.gov.gds.ier.step.{OrdinaryStep, Routes}
+import uk.gov.gds.ier.step.OrdinaryStep
+import play.api.mvc.Call
+import uk.gov.gds.ier.step.Routes
+import uk.gov.gds.ier.model.PostalVote
+import uk.gov.gds.ier.model.InprogressOrdinary
+import uk.gov.gds.ier.validation.InProgressForm
+import scala.Some
 
 class PostalVoteStep @Inject ()(val serialiser: JsonSerialiser,
                                       val config: Config,
@@ -30,6 +33,47 @@ class PostalVoteStep @Inject ()(val serialiser: JsonSerialiser,
     editGet = PostalVoteController.editGet,
     editPost = PostalVoteController.editPost
   )
+
+  override def get(implicit manifest: Manifest[InprogressOrdinary]) = ValidSession requiredFor {
+    request => application =>
+      logger.debug(s"GET request for ${request.path}")
+      Ok(template(InProgressForm(validation.fill(prepopulateEmailAddress(application))), routes.post, previousRoute))
+  }
+
+  def prepopulateEmailAddress (application:InprogressOrdinary):InprogressOrdinary = {
+    application.contact match {
+      case Some(Contact(_,_,Some(ContactDetail(_,Some(emailAddress))))) => {
+        application.postalVote match {
+          case Some(PostalVote(postalVoteOption, deliveryMethod)) => {
+           deliveryMethod match {
+              case Some(PostalVoteDeliveryMethod(deliveryMethod,postalVoteMailAddress)) =>  {
+                if (!postalVoteMailAddress.isDefined) {
+                  val updatedApplication = application.copy(
+                    postalVote = Some(PostalVote(postalVoteOption, Some(PostalVoteDeliveryMethod(deliveryMethod, Some(emailAddress)))))
+                  )
+                  updatedApplication
+                }
+                else application
+              }
+              case None => {
+                val updatedApplication = application.copy(
+                  postalVote = Some(PostalVote(postalVoteOption, Some(PostalVoteDeliveryMethod(None, Some(emailAddress)))))
+                )
+                updatedApplication
+              }
+            }
+          }
+          case None => {
+            val updatedApplication = application.copy(
+              postalVote = Some(PostalVote(false, Some(PostalVoteDeliveryMethod(None,Some(emailAddress)))))
+            )
+            updatedApplication
+          }
+        }
+      }
+      case _ => application
+    }
+  }
 
   def template(form:InProgressForm[InprogressOrdinary], call:Call, backUrl: Option[Call]): Html = {
     views.html.steps.postalVote(form, call, backUrl.map(_.url))
