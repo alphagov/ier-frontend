b67186d802c0ce333d5eef6d21ffa227a1ae08ad
Antonio Perez <a.perez@kainos.com>
2014-01-29 15:53:48 +0000

    moved validation constraint to exit condition

--- app/uk/gov/gds/ier/validation/constraints/overseas/DateLeftUkConstraints.scala
+++ app/uk/gov/gds/ier/validation/constraints/overseas/DateLeftUkConstraints.scala
@@ -5,7 +5,6 @@ import play.api.data.validation.{Invalid, Valid, Constraint}
 import uk.gov.gds.ier.model._
 import scala.Some
 import uk.gov.gds.ier.validation.constraints.CommonConstraints
-import org.joda.time.{DateTime, Months}
 
 trait DateLeftUkConstraints extends CommonConstraints{
   self: ErrorMessages
@@ -13,25 +12,8 @@ trait DateLeftUkConstraints extends CommonConstraints{
 
   lazy val validateDateLeftUk = Constraint[InprogressOverseas](keys.dateLeftUk.key) {
     application => application.dateLeftUk match {
-      case Some(dateLeftUk) => validateDateLeftUkNotOver15Years(dateLeftUk)
-      case None => Invalid(
-        "Please answer this question",
-        keys.dateLeftUk.month,
-        keys.dateLeftUk.year
-      )
+      case Some(dateLeftUk) => Valid
+      case None => Invalid ("Please answer this question", keys.dateLeftUk.month, keys.dateLeftUk.year)
     }
-  }
-
-  def validateDateLeftUkNotOver15Years(dateLeftUk:DateLeftUk) = {
-    val leftUk = new DateTime().withMonthOfYear(dateLeftUk.month).withYear(dateLeftUk.year)
-    val monthDiff = Months.monthsBetween(leftUk, DateTime.now()).getMonths()
-    if (monthDiff >= 15 * 12) {
-      Invalid(
-        "You cannot register, because it has been over 15 years since you left the UK",
-        keys.dateLeftUk.month,
-        keys.dateLeftUk.year
-      )
-    }
-    else Valid
   }
 }
