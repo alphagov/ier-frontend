934045109eb7f91168c7efc54aa7aed9efedccb7
Jianfeng Tian <jianfeng.tian@gmail.com>
2014-02-03 18:19:58 +0000

    add tempalte test

--- test/assets/mustache/overseas/AddressTemplateTest.scala
+++ test/assets/mustache/overseas/AddressTemplateTest.scala
@@ -31,35 +31,36 @@ class AddressTemplateTest
         address = Field(
           id = "overseasAddressDetailsId",
           name = "overseasAddressDetailsName",
-          classes = "overseasAddressDetailsClasses",
+          classes = "overseasAddressDetailsClass",
           value = "some address"
         )
       )
 
       val html = Mustache.render("overseas/address", data)
-      println (html)
       val doc = Jsoup.parse(html.toString)
+      println (doc)
 
       //country select
-      doc.select("label[for=overseasAddressCountryId]").size() should be (1)
-
-      val countrySelectDiv = doc.select("div[class*=overseasAddressClass]")
-      countrySelectDiv.size() should be (1)
-      val countrySelect = countrySelectDiv.select("select").first()
+      val abc = doc.select("label[for=overseasAddressCountryId]").size() 
+      abc should be (1)
+      
+      val countrySelectDiv = doc.select("div[class*=overseasAddressCountryClass]").first()
+      
+      val countrySelect = countrySelectDiv.select("select")
 //      countrySelect.attr("id") should be("overseasAddressCountryId")
 //      countrySelect.attr("name") should be("overseasAddressCountryName")
 //      countrySelect.attr("class") should include("overseasAddressCountryClass")
 //      countrySelect.attr("value") should be("United Kingdom")
-//
-//
-//      //address details
-//      doc.select("label[for=overseasAddressDetailsId]").size should be (1)
-//
-//      val addressDetailsInput = doc.select("input[id=overseasAddressDetailsId]").first()
-//      addressDetailsInput.attr("id") should be("overseasAddressDetailsId")
-//      addressDetailsInput.attr("name") should be("overseasAddress.overseasAddressDetailsName")
-//      addressDetailsInput.attr("value") should be("some address")
-//      addressDetailsInput.attr("class") should include("overseasAddressDetailsName")
+
+
+      //address details
+      doc.select("label[for=overseasAddressDetailsId]").size should be (1)
+
+      val addressDetailsInput = doc.select("textarea[id=overseasAddressDetailsId]").first()
+      addressDetailsInput.attr("id") should be("overseasAddressDetailsId")
+      addressDetailsInput.attr("name") should be("overseasAddressDetailsName")
+      addressDetailsInput.text should be("some address")
+      addressDetailsInput.attr("class") should include("overseasAddressDetailsClass")
     }
   }
 }
