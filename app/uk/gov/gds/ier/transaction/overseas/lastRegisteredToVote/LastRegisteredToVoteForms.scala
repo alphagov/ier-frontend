4fd6b3081d9baa0fbd71b17d70d563fe1b60806a
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-02-07 15:09:03 +0000

    Antonio's PR feedback Remove most of TODOs

--- app/uk/gov/gds/ier/transaction/overseas/lastRegisteredToVote/LastRegisteredToVoteForms.scala
+++ app/uk/gov/gds/ier/transaction/overseas/lastRegisteredToVote/LastRegisteredToVoteForms.scala
@@ -6,8 +6,7 @@ import uk.gov.gds.ier.validation.constraints.DateOfBirthConstraints
 import play.api.data.Forms._
 import scala.Some
 
-//TODO: is extending DateOfBirthConstraints really correct?
-trait LastRegisteredToVoteForms extends DateOfBirthConstraints {
+trait LastRegisteredToVoteForms {
     self:  FormKeys
       with ErrorMessages =>
 
--- app/uk/gov/gds/ier/transaction/overseas/waysToVote/WaysToVoteForms.scala
+++ app/uk/gov/gds/ier/transaction/overseas/waysToVote/WaysToVoteForms.scala
@@ -3,9 +3,8 @@ package uk.gov.gds.ier.transaction.overseas.waysToVote
 import uk.gov.gds.ier.validation.{ErrorTransformForm, ErrorMessages, FormKeys}
 import play.api.data.Forms._
 import uk.gov.gds.ier.model.{WaysToVoteType, WaysToVote, InprogressOverseas}
-import uk.gov.gds.ier.validation.constraints.overseas.WaysToVoteConstraints
 
-trait WaysToVoteForms extends WaysToVoteConstraints {
+trait WaysToVoteForms {
   self:  FormKeys
     with ErrorMessages =>
 
