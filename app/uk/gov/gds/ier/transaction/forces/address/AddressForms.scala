1fd75d5016fde55348d17c803d1c1c1ef53713b4
Antonio Perez <a.perez@kainos.com>
2014-02-21 12:09:55 +0000

    in progress

---
+++ app/uk/gov/gds/ier/transaction/forces/address/AddressForms.scala
@@ -0,0 +1,172 @@
+package uk.gov.gds.ier.transaction.forces.address
+
+import play.api.data.Forms._
+import play.api.data.validation.{Invalid, Valid, Constraint}
+import uk.gov.gds.ier.validation.{
+  ErrorTransformForm,
+  ErrorMessages,
+  FormKeys,
+  PostcodeValidator}
+import uk.gov.gds.ier.validation.constraints.CommonConstraints
+import uk.gov.gds.ier.serialiser.WithSerialiser
+import uk.gov.gds.ier.model.{
+  InprogressOverseas,
+  PartialAddress,
+  PossibleAddress,
+  Addresses}
+
+trait LastUkAddressForms extends LastUkAddressConstraints {
+  self: FormKeys
+  with ErrorMessages
+  with WithSerialiser =>
+
+  lazy val partialAddressMapping = mapping(
+    keys.addressLine.key -> optional(nonEmptyText),
+    keys.uprn.key -> optional(nonEmptyText),
+    keys.postcode.key -> nonEmptyText,
+    keys.manualAddress.key -> optional(nonEmptyText)
+  ) (
+    PartialAddress.apply
+  ) (
+    PartialAddress.unapply
+  ).verifying(manualAddressMaxLength, postcodeIsValid, uprnOrManualDefined)
+
+  lazy val manualPartialAddressMapping = mapping(
+    keys.postcode.key -> nonEmptyText,
+    keys.manualAddress.key -> optional(nonEmptyText)
+  ) (
+    (postcode, manualAddress) => PartialAddress(
+      addressLine = None,
+      uprn = None,
+      postcode = postcode,
+      manualAddress = manualAddress
+    )
+  ) (
+    partial => Some(
+      partial.postcode,
+      partial.manualAddress
+    )
+  ).verifying(manualAddressMaxLength, postcodeIsValid)
+
+  lazy val postcodeLookupMapping = mapping(
+    keys.postcode.key -> nonEmptyText
+  ) (
+    postcode => PartialAddress(
+      addressLine = None,
+      uprn = None,
+      postcode = postcode,
+      manualAddress = None
+    )
+  ) (
+    partial => Some(partial.postcode)
+  ).verifying(postcodeIsValid)
+
+  lazy val possibleAddressesMapping = mapping(
+    keys.jsonList.key -> nonEmptyText,
+    keys.postcode.key -> nonEmptyText
+  ) (
+    (json, postcode) => PossibleAddress(
+      serialiser.fromJson[Addresses](json),
+      postcode
+    )
+  ) (
+    possibleAddress => Some(
+      serialiser.toJson(possibleAddress.jsonList),
+      possibleAddress.postcode
+    )
+  )
+
+  val lookupAddressForm = ErrorTransformForm(
+    mapping (
+      keys.lastUkAddress.key -> optional(postcodeLookupMapping)
+    ) (
+      lastUkAddr => InprogressOverseas(
+        lastUkAddress = lastUkAddr
+      )
+    ) (
+      inprogress => Some(inprogress.lastUkAddress)
+    ).verifying( postcodeIsNotEmpty )
+  )
+
+  val lastUkAddressForm = ErrorTransformForm(
+    mapping (
+      keys.lastUkAddress.key -> optional(partialAddressMapping),
+      keys.possibleAddresses.key -> optional(possibleAddressesMapping)
+    ) (
+      (lastUkAddr, possibleAddr) => InprogressOverseas(
+        lastUkAddress = lastUkAddr,
+        possibleAddresses = possibleAddr
+      )
+    ) (
+      inprogress => Some(
+        inprogress.lastUkAddress,
+        inprogress.possibleAddresses
+      )
+    ).verifying( addressIsRequired )
+  )
+
+  val manualAddressForm = ErrorTransformForm(
+    mapping(
+      keys.lastUkAddress.key -> optional(manualPartialAddressMapping)
+    ) (
+      lastUkAddr => InprogressOverseas(lastUkAddress = lastUkAddr)
+    ) (
+      inprogress => Some(inprogress.lastUkAddress)
+    ).verifying( manualAddressIsRequired )
+  )
+}
+
+
+trait LastUkAddressConstraints extends CommonConstraints {
+  self: FormKeys
+   with ErrorMessages =>
+
+  lazy val manualAddressIsRequired = Constraint[InprogressOverseas](keys.lastUkAddress.key) {
+    inprogress =>
+      inprogress.lastUkAddress match {
+        case Some(partialAddress) if partialAddress.manualAddress.isDefined => Valid
+        case _ => Invalid("Please answer this question", keys.lastUkAddress.manualAddress)
+      }
+  }
+
+  lazy val postcodeIsNotEmpty = Constraint[InprogressOverseas](keys.lastUkAddress.key) {
+    inprogress =>
+      inprogress.lastUkAddress match {
+        case Some(partialAddress) if partialAddress.postcode == "" => {
+          Invalid("Please enter your postcode", keys.lastUkAddress.postcode)
+        }
+        case None => Invalid("Please enter your postcode", keys.lastUkAddress.postcode)
+        case _ => Valid
+      }
+  }
+
+  lazy val addressIsRequired = Constraint[InprogressOverseas](keys.lastUkAddress.key) {
+    inprogress =>
+      if (inprogress.lastUkAddress.isDefined) Valid
+      else Invalid("Please answer this question", keys.lastUkAddress)
+  }
+
+  lazy val uprnOrManualDefined = Constraint[PartialAddress](keys.lastUkAddress.key) {
+    case partialAddress if partialAddress.uprn.exists(_ != "") => Valid
+    case partialAddress if partialAddress.manualAddress.exists(_ != "") => Valid
+    case _ => Invalid(
+      "Please select your address",
+      keys.lastUkAddress.uprn,
+      keys.lastUkAddress.manualAddress,
+      keys.lastUkAddress
+    )
+  }
+
+  lazy val manualAddressMaxLength = Constraint[PartialAddress](keys.lastUkAddress.key) {
+    case PartialAddress(_, _, _, Some(manualAddress))
+      if manualAddress.size <= maxExplanationFieldLength => Valid
+    case PartialAddress(_, _, _, None) => Valid
+    case _ => Invalid(addressMaxLengthError, keys.lastUkAddress.manualAddress)
+  }
+
+  lazy val postcodeIsValid = Constraint[PartialAddress](keys.lastUkAddress.key) {
+    case PartialAddress(_, _, postcode, _)
+      if PostcodeValidator.isValid(postcode) => Valid
+    case _ => Invalid("Your postcode is not valid", keys.lastUkAddress.postcode)
+  }
+}
---
+++ app/uk/gov/gds/ier/transaction/forces/address/AddressSelectStep.scala
@@ -0,0 +1,98 @@
+package uk.gov.gds.ier.transaction.forces.address
+
+import controllers.step.overseas.routes.{
+  LastUkAddressController,
+  LastUkAddressManualController,
+  LastUkAddressSelectController,
+  DateLeftUkController}
+import controllers.step.overseas.NameController
+import com.google.inject.Inject
+import play.api.mvc.Call
+import uk.gov.gds.ier.config.Config
+import uk.gov.gds.ier.model.{
+  InprogressOverseas,
+  Addresses,
+  PossibleAddress}
+import uk.gov.gds.ier.security.{EncryptionKeys, EncryptionService}
+import uk.gov.gds.ier.serialiser.JsonSerialiser
+import uk.gov.gds.ier.service.AddressService
+import uk.gov.gds.ier.step.{OverseaStep, Routes}
+import uk.gov.gds.ier.validation.InProgressForm
+
+class AddressSelectStep @Inject() (
+    val serialiser: JsonSerialiser,
+    val config: Config,
+    val encryptionService: EncryptionService,
+    val encryptionKeys: EncryptionKeys,
+    val addressService: AddressService)
+  extends OverseaStep
+  with AddressMustache
+  with LastUkAddressForms {
+
+  val validation = lastUkAddressForm
+
+  val previousRoute = Some(DateLeftUkController.get)
+
+  val routes = Routes(
+    get = LastUkAddressSelectController.get,
+    post = LastUkAddressSelectController.post,
+    editGet = LastUkAddressSelectController.editGet,
+    editPost = LastUkAddressSelectController.editPost
+  )
+
+  def nextStep(currentState: InprogressOverseas) = {
+    NameController.nameStep
+  }
+
+  override def postSuccess(currentState: InprogressOverseas) = {
+    val addressWithAddressLine = currentState.lastUkAddress.map {
+      addressService.fillAddressLine(_)
+    }
+
+    currentState.copy(
+      lastUkAddress = addressWithAddressLine,
+      possibleAddresses = None
+    )
+  }
+
+  def template(
+      form: InProgressForm[InprogressOverseas],
+      call: Call,
+      backUrl: Option[Call]) = {
+
+    val storedAddresses = for(
+      jsonList <- form(keys.possibleAddresses.jsonList).value;
+      postcode <- form(keys.possibleAddresses.postcode).value
+    ) yield {
+      PossibleAddress(
+        jsonList = serialiser.fromJson[Addresses](jsonList),
+        postcode = postcode
+      )
+    }
+
+    val maybeAddresses = storedAddresses.orElse {
+      lookupAddresses(form(keys.lastUkAddress.postcode).value)
+    }
+
+    LastUkAddressMustache.selectPage(
+      form,
+      backUrl.map(_.url).getOrElse(""),
+      call.url,
+      LastUkAddressController.get.url,
+      LastUkAddressManualController.get.url,
+      maybeAddresses
+    )
+  }
+
+  private[lastUkAddress] def lookupAddresses(
+      maybePostcode:Option[String]): Option[PossibleAddress] = {
+
+    maybePostcode.map { postcode =>
+      val addresses = addressService.lookupPartialAddress(postcode)
+      PossibleAddress(
+        jsonList = Addresses(addresses),
+        postcode = postcode
+      )
+    }
+  }
+}
