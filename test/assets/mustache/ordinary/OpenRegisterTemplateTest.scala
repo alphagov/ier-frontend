5934511b3b1e4745d3883f6cfbf0b851e30fdf32
Jianfeng Tian <jianfeng.tian@gmail.com>
2014-02-26 16:21:51 +0000

    add template test

---
+++ test/assets/mustache/ordinary/OpenRegisterTemplateTest.scala
@@ -0,0 +1,39 @@
+package assets.mustache.ordinary
+
+import org.scalatest.{Matchers, FlatSpec}
+import play.api.test._
+import org.jsoup.Jsoup
+import play.api.test.Helpers._
+import uk.gov.gds.ier.transaction.ordinary.openRegister.OpenRegisterMustache
+
+class OpenRegisterTemplateTest
+  extends FlatSpec
+  with OpenRegisterMustache
+  with Matchers {
+
+  it should "properly render all properties from the model" in {
+
+    running(FakeApplication()) {
+      val data = OpenRegisterModel(
+        question = Question(postUrl = "/whatever-url",
+        backUrl = "",
+        number = "1",
+        title = "open register title"
+        ),
+        openRegister = Field(
+          id = "openRegister_optIn",
+          name = "openRegister.optIn",
+          value = "false"
+        )
+      )
+
+      val html = Mustache.render("ordinary/openRegister", data)
+      val doc = Jsoup.parse(html.toString)
+
+      val openRegisterInput = doc.select("input[id=openRegister_optIn]").first()
+      openRegisterInput.attr("id") should be("openRegister_optIn")
+      openRegisterInput.attr("name") should be("openRegister.optIn")
+      openRegisterInput.attr("value") should be("false")
+    }
+  }
+}
