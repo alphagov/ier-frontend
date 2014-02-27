eb190ee5250258f98f073deaacaca0ee5eb62561
Antonio Perez <a.perez@kainos.com>
2014-02-27 10:20:17 +0000

    new validations defined for confirmation form

--- test/uk/gov/gds/ier/transaction/forces/confirmation/ConfirmationFormTests.scala
+++ test/uk/gov/gds/ier/transaction/forces/confirmation/ConfirmationFormTests.scala
@@ -21,16 +21,50 @@ class ConfirmationFormTests
   it should "error out on empty json" in {
     val js = JsNull
     confirmationForm.bind(js).fold(
-      hasErrors => fail("Should have NOT errored out."),
-      success => success should not be None
+      hasErrors => {
+        val errorMessage = Seq("Please complete this step")
+        hasErrors.errorMessages("statement") should be(errorMessage)
+        hasErrors.errorMessages("address") should be(errorMessage)
+        hasErrors.errorMessages("nationality") should be(errorMessage)
+        hasErrors.errorMessages("dob") should be(errorMessage)
+        hasErrors.errorMessages("name") should be(errorMessage)
+        hasErrors.errorMessages("NINO") should be(errorMessage)
+        hasErrors.errorMessages("service") should be(errorMessage)
+        hasErrors.errorMessages("rank") should be(errorMessage)
+        hasErrors.errorMessages("contactAddress") should be(errorMessage)
+        hasErrors.errorMessages("openRegister") should be(errorMessage)
+        hasErrors.errorMessages("waysToVote") should be(errorMessage)
+        hasErrors.errorMessages("postalOrProxyVote") should be(errorMessage)
+        hasErrors.errorMessages("contact") should be(errorMessage)
+        hasErrors.globalErrorMessages.count(_ == "Please complete this step") should be(13)
+        hasErrors.errors.size should be(26)
+      },
+      success => fail("Should have errored out.")
     )
   }
 
   it should "error out on empty application" in {
     val application = InprogressForces()
     confirmationForm.fillAndValidate(application).fold(
-      hasErrors => fail("Should have NOT errored out."),
-      success => success should not be None
+      hasErrors => {
+        val errorMessage = Seq("Please complete this step")
+        hasErrors.errorMessages("statement") should be(errorMessage)
+        hasErrors.errorMessages("address") should be(errorMessage)
+        hasErrors.errorMessages("nationality") should be(errorMessage)
+        hasErrors.errorMessages("dob") should be(errorMessage)
+        hasErrors.errorMessages("name") should be(errorMessage)
+        hasErrors.errorMessages("NINO") should be(errorMessage)
+        hasErrors.errorMessages("service") should be(errorMessage)
+        hasErrors.errorMessages("rank") should be(errorMessage)
+        hasErrors.errorMessages("contactAddress") should be(errorMessage)
+        hasErrors.errorMessages("openRegister") should be(errorMessage)
+        hasErrors.errorMessages("waysToVote") should be(errorMessage)
+        hasErrors.errorMessages("postalOrProxyVote") should be(errorMessage)
+        hasErrors.errorMessages("contact") should be(errorMessage)
+        hasErrors.globalErrorMessages.count(_ == "Please complete this step") should be(13)
+        hasErrors.errors.size should be(26)
+      },
+      success => fail("Should have errored out.")
     )
   }
 }
