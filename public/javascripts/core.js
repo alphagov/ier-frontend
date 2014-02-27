4aa8b4c5ffba55caa70aae0ab3d88a2feff434cb
Tom Byers <tombaromba@gmail.com>
2014-02-27 18:25:07 +0000

    Move toggled content sections JS to global
    Needs to be used on start page.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -5,6 +5,88 @@ window.GOVUK.registerToVote = {};
 (function () {
   "use strict";
 
+  var root = this,
+      $ = root.jQuery,
+      GOVUK = root.GOVUK,
+      ToggleObj,
+      OptionalInformation,
+      $searchFocus;
+
+  // Base contructor for content associated with a 'toggle' element that controls its visiblity
+  ToggleObj = function (elm, toggleClass) {
+    if (elm) {
+      this.$content = $(elm);
+      this.toggleActions = {
+        'hidden': 'Expand',
+        'visible': 'Hide'
+      };
+      this.toggleClass = toggleClass + '-open';
+      this.setup();
+      this.bindEvents();
+    }
+  };
+  ToggleObj.prototype.setAccessibilityAPI = function (state) {
+    if (state === 'hidden') {
+      this.$content.attr('aria-hidden', true);
+      this.$content.attr('aria-expanded', false);
+      this.$toggle.find('span.visuallyhidden').eq(0).text(this.toggleActions.hidden);
+    } else {
+      this.$content.attr('aria-hidden', false);
+      this.$content.attr('aria-expanded', true);
+      this.$toggle.find('span.visuallyhidden').eq(0).text(this.toggleActions.visible);
+    }
+  };
+  ToggleObj.prototype.toggle = function () {
+    if (this.$content.css("display") === "none") {
+      this.$content.addClass(this.toggleClass);
+      this.setAccessibilityAPI('visible');
+      this.$toggle.removeClass("toggle-closed");
+      this.$toggle.addClass("toggle-open");
+      $(document).trigger('toggle.open', { '$toggle' : this.$toggle });
+    } else {
+      this.$content.removeClass(this.toggleClass);
+      this.setAccessibilityAPI('hidden');
+      this.$toggle.removeClass("toggle-open");
+      this.$toggle.addClass("toggle-closed");
+      $(document).trigger('toggle.closed', { '$toggle' : this.$toggle });
+    }
+  };
+  ToggleObj.prototype.setInitialState = function () {
+    if (this.$content.hasClass(this.toggleClass)) {
+      this.$toggle.addClass('toggle-open');
+      this.$toggle.removeClass('toggle-closed');
+      this.setAccessibilityAPI('visible');
+    }
+  };
+
+  // Constructor for controlling the display of content that is additional to the main block
+  // Uses ToggleObj for its base prototype
+  OptionalInformation = function () {
+    ToggleObj.apply(this, arguments);
+  };
+  $.extend(OptionalInformation.prototype, new ToggleObj());
+  OptionalInformation.prototype.setup = function () {
+    var contentId = this.$content.attr('id'),
+        headingText;
+
+    this.$heading = this.$content.find("h1,h2,h3,h4").first();
+    this.$toggle = $('<a href="#" class="toggle toggle-closed"><span class="visuallyhidden">Show</span> ' + this.$heading.text() + ' <span class="visuallyhidden">section</span></a>');
+    if (contentId) { this.$toggle.attr('aria-controls', contentId); }
+    this.$toggle.insertBefore(this.$content);
+    this.$heading.addClass("visuallyhidden");
+    this.setInitialState();
+  };
+  OptionalInformation.prototype.bindEvents = function () {
+    var _this = this;
+
+    this.$toggle.on('click', function () {
+      _this.toggle();
+      return false;
+    });
+  };
+
+  GOVUK.registerToVote.OptionalInformation = OptionalInformation;
+
   // header search toggle
   $('.js-header-toggle').on('click', function(e) {
     e.preventDefault();
@@ -12,7 +94,7 @@ window.GOVUK.registerToVote = {};
     $(this).toggleClass('js-hidden');
   });
 
-  var $searchFocus = $('.js-search-focus');
+  $searchFocus = $('.js-search-focus');
   $searchFocus.each(function(i, el){
     if($(el).val() !== ''){
       $(el).addClass('focus');
@@ -25,5 +107,9 @@ window.GOVUK.registerToVote = {};
     if($(e.target).val() === ''){
       $(e.target).removeClass('focus');
     }
+  });
+
+  $('.help-content').each(function (idx, elm) {
+    new GOVUK.registerToVote.OptionalInformation(elm, 'help-content');
   });
 }.call(this));
--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -7,8 +7,6 @@
       countries = GOVUK.registerToVote.countries,
       validation = GOVUK.registerToVote.validation,
       ENTER = 13,
-      ToggleObj,
-      OptionalInformation,
       ConditionalControl,
       DuplicateField,
       MarkSelected,
@@ -36,79 +34,6 @@
     this.$link.on("click", function(e) {
       e.preventDefault();
       root.history.back();
-      return false;
-    });
-  };
-
-  // Base contructor for content associated with a 'toggle' element that controls its visiblity
-  ToggleObj = function (elm, toggleClass) {
-    if (elm) {
-      this.$content = $(elm);
-      this.toggleActions = {
-        'hidden': 'Expand',
-        'visible': 'Hide'
-      };
-      this.toggleClass = toggleClass + '-open';
-      this.setup();
-      this.bindEvents();
-    }
-  };
-  ToggleObj.prototype.setAccessibilityAPI = function (state) {
-    if (state === 'hidden') {
-      this.$content.attr('aria-hidden', true);
-      this.$content.attr('aria-expanded', false);
-      this.$toggle.find('span.visuallyhidden').eq(0).text(this.toggleActions.hidden);
-    } else {
-      this.$content.attr('aria-hidden', false);
-      this.$content.attr('aria-expanded', true);
-      this.$toggle.find('span.visuallyhidden').eq(0).text(this.toggleActions.visible);
-    }
-  };
-  ToggleObj.prototype.toggle = function () {
-    if (this.$content.css("display") === "none") {
-      this.$content.addClass(this.toggleClass);
-      this.setAccessibilityAPI('visible');
-      this.$toggle.removeClass("toggle-closed");
-      this.$toggle.addClass("toggle-open");
-      $(document).trigger('toggle.open', { '$toggle' : this.$toggle });
-    } else {
-      this.$content.removeClass(this.toggleClass);
-      this.setAccessibilityAPI('hidden');
-      this.$toggle.removeClass("toggle-open");
-      this.$toggle.addClass("toggle-closed");
-      $(document).trigger('toggle.closed', { '$toggle' : this.$toggle });
-    }
-  };
-  ToggleObj.prototype.setInitialState = function () {
-    if (this.$content.hasClass(this.toggleClass)) {
-      this.$toggle.addClass('toggle-open');
-      this.$toggle.removeClass('toggle-closed');
-      this.setAccessibilityAPI('visible');
-    }
-  };
-
-  // Constructor for controlling the display of content that is additional to the main block
-  // Uses ToggleObj for its base prototype
-  OptionalInformation = function () {
-    ToggleObj.apply(this, arguments);
-  };
-  $.extend(OptionalInformation.prototype, new ToggleObj());
-  OptionalInformation.prototype.setup = function () {
-    var contentId = this.$content.attr('id'),
-        headingText;
-
-    this.$heading = this.$content.find("h1,h2,h3,h4").first();
-    this.$toggle = $('<a href="#" class="toggle toggle-closed"><span class="visuallyhidden">Show</span> ' + this.$heading.text() + ' <span class="visuallyhidden">section</span></a>');
-    if (contentId) { this.$toggle.attr('aria-controls', contentId); }
-    this.$toggle.insertBefore(this.$content);
-    this.$heading.addClass("visuallyhidden");
-    this.setInitialState();
-  };
-  OptionalInformation.prototype.bindEvents = function () {
-    var _this = this;
-
-    this.$toggle.on('click', function () {
-      _this.toggle();
       return false;
     });
   };
