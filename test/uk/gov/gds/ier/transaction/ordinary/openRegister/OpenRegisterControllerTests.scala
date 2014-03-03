e0abcfb5ec4a14dede2f56a581979c2fbc2b5fd3
Jianfeng Tian <jianfeng.tian@gmail.com>
2014-03-03 11:45:56 +0000

    remove the back link

--- test/uk/gov/gds/ier/transaction/ordinary/openRegister/OpenRegisterControllerTests.scala
+++ test/uk/gov/gds/ier/transaction/ordinary/openRegister/OpenRegisterControllerTests.scala
@@ -24,8 +24,6 @@ class OpenRegisterControllerTests
       status(result) should be(OK)
       contentType(result) should be(Some("text/html"))
       contentAsString(result) should include("Question 9")
-      contentAsString(result) should include(
-        "<a class=\"back-to-previous\" href=\"/register-to-vote/previous-address")
       contentAsString(result) should include("Do you want to include your name and address on the open register?")
       contentAsString(result) should include("/register-to-vote/open-register")
     }
@@ -97,7 +95,6 @@ class OpenRegisterControllerTests
       status(result) should be(OK)
       contentType(result) should be(Some("text/html"))
       contentAsString(result) should include("Question 9")
-      contentAsString(result) should include("<a class=\"back-to-previous\" href=\"/register-to-vote/confirmation")
       contentAsString(result) should include("Do you want to include your name and address on the open register?")
       contentAsString(result) should include("/register-to-vote/edit/open-register")
     }
