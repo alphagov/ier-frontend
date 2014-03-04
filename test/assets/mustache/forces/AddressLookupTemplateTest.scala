577bb0d072641b6b6095f205c064f6fa5b508742
Antonio Perez <a.perez@kainos.com>
2014-03-04 15:15:30 +0000

    additional tests

---
+++ test/assets/mustache/forces/AddressLookupTemplateTest.scala
@@ -0,0 +1,52 @@
+package assets.mustache.forces
+
+import org.jsoup.Jsoup
+import org.scalatest.{Matchers, FlatSpec}
+import play.api.test._
+import play.api.test.Helpers._
+import uk.gov.gds.ier.serialiser.WithSerialiser
+import uk.gov.gds.ier.test.TestHelpers
+import uk.gov.gds.ier.transaction.forces.address.AddressMustache
+
+class AddressLookupTemplateTest
+  extends FlatSpec
+  with AddressMustache
+  with Matchers
+  with WithSerialiser
+  with TestHelpers {
+
+  val serialiser = jsonSerialiser
+
+  import AddressMustache._
+
+  it should "properly render" in {
+    running(FakeApplication()) {
+      val data = new LookupModel(
+        question = Question(),
+        postcode = Field(
+          id = "postcodeId",
+          name = "postcodeName",
+          classes = "postcodeClasses",
+          value = "postcodeValue"
+        )
+      )
+
+      val html = Mustache.render("forces/addressLookup", data)
+      val doc = Jsoup.parse(html.toString)
+
+      val fieldset = doc.select("fieldset").first()
+
+      val label = fieldset.select("label").first()
+      label.attr("for") should be("postcodeId")
+
+      val divWrapper = fieldset.select("div").first()
+      divWrapper.attr("class") should include("postcodeClasses")
+      
+      val input = divWrapper.select("input").first()
+      input.attr("id") should be("postcodeId")
+      input.attr("name") should be("postcodeName")
+      input.attr("value") should be("postcodeValue")
+      input.attr("class") should include("postcodeClasses")
+    }
+  }
+}
---
+++ test/assets/mustache/forces/AddressManualTemplateTest.scala
@@ -0,0 +1,69 @@
+package assets.mustache.forces
+
+import org.jsoup.Jsoup
+import org.scalatest.{Matchers, FlatSpec}
+import play.api.test._
+import play.api.test.Helpers._
+import uk.gov.gds.ier.serialiser.WithSerialiser
+import uk.gov.gds.ier.test.TestHelpers
+import uk.gov.gds.ier.transaction.forces.address.AddressMustache
+
+class AddressManualTemplateTest
+  extends FlatSpec
+  with AddressMustache
+  with Matchers
+  with WithSerialiser
+  with TestHelpers {
+
+  val serialiser = jsonSerialiser
+
+  import AddressMustache._
+
+  it should "properly render" in {
+    running(FakeApplication()) {
+      val data = new ManualModel(
+        question = Question(),
+        lookupUrl = "http://lookup",
+        postcode = Field(
+          id = "postcodeId",
+          name = "postcodeName",
+          classes = "postcodeClasses",
+          value = "postcodeValue"
+        ),
+        manualAddress = Field(
+          id = "manualId",
+          name = "manualName",
+          classes = "manualClasses",
+          value = "manualValue"
+        )
+      )
+
+      val html = Mustache.render("forces/addressManual", data)
+      val doc = Jsoup.parse(html.toString)
+
+      val fieldset = doc.select("fieldset").first()
+
+      val postcodeSpan = fieldset.select("span[class=postcode]").first()
+      postcodeSpan.html() should be("postcodeValue")
+
+      val postcodeInput = fieldset.select("input[type=hidden]").first()
+      postcodeInput.attr("id") should be("postcodeId")
+      postcodeInput.attr("name") should be("postcodeName")
+      postcodeInput.attr("value") should be("postcodeValue")
+
+      val manualLabel = fieldset.select("label[for=manualId]")
+      manualLabel.attr("for") should be("manualId")
+
+      val divWrapper = fieldset.select("div").first()
+      divWrapper.attr("class") should include("manualClasses")
+
+      val manualText = divWrapper.select("textarea").first()
+      manualText.attr("id") should be("manualId")
+      manualText.attr("name") should be("manualName")
+      manualText.attr("class") should include("manualClasses")
+
+      val lookupChangeLink = fieldset.select("a").first()
+      lookupChangeLink.attr("href") should be("http://lookup")
+    }
+  }
+}
