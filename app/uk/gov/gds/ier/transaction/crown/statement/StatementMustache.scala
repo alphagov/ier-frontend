6395eb42c8982544960d4f3c82aeca84f1dabd49
michaeldfallen <michaeldfallen@gmail.com>
2014-03-17 16:02:30 +0000

    statement mustache data binding and tests complete

--- app/uk/gov/gds/ier/transaction/crown/statement/StatementMustache.scala
+++ app/uk/gov/gds/ier/transaction/crown/statement/StatementMustache.scala
@@ -10,11 +10,39 @@ trait StatementMustache extends StepMustache {
 
   case class StatementModel(
       question: Question,
-      crown:Field,
-      crownServant:Field,
-      crownPartner:Field,
-      council:Field,
-      councilEmployee:Field,
-      councilPartner:Field
+      crown: Field,
+      crownServant: Field,
+      crownPartner: Field,
+      council: Field,
+      councilEmployee: Field,
+      councilPartner: Field
   )
+
+  def statementData(
+      form: ErrorTransformForm[InprogressCrown],
+      postEndpoint: Call,
+      backEndpoint: Option[Call]) = {
+    implicit val progressForm = form
+    StatementModel(
+      question = Question(
+        postUrl = postEndpoint.url,
+        backUrl = backEndpoint.map { _.url }.getOrElse(""),
+        errorMessages = form.globalErrors.map { _.message },
+        number = "1",
+        title = "Which of these statements applies to you?"
+      ),
+      crown = Field(
+        id = "crown" + keys.statement.key,
+        classes = if (form(keys.statement.key).hasErrors) "invalid" else ""
+      ),
+      crownServant = CheckboxField(keys.statement.crownServant, "true"),
+      crownPartner = CheckboxField(keys.statement.crownPartner, "true"),
+      council = Field(
+        id = "council" + keys.statement.key,
+        classes = if (form(keys.statement.key).hasErrors) "invalid" else ""
+      ),
+      councilEmployee = CheckboxField(keys.statement.councilEmployee, "true"),
+      councilPartner = CheckboxField(keys.statement.councilPartner, "true")
+    )
+  }
 }
