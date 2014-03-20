275d5e46a9996b148510c63e6dbf4920107d0ea3
Antonio Perez <a.perez@kainos.com>
2014-03-20 17:44:04 +0000

    some missing tests

---
+++ test/uk/gov/gds/ier/transaction/crown/address/AddressFirstMustacheTest.scala
@@ -0,0 +1,63 @@
+package uk.gov.gds.ier.transaction.crown.address
+
+import org.scalatest.{Matchers, FlatSpec}
+import uk.gov.gds.ier.validation.{FormKeys, ErrorMessages}
+import uk.gov.gds.ier.test.TestHelpers
+import uk.gov.gds.ier.serialiser.WithSerialiser
+import uk.gov.gds.ier.model._
+import uk.gov.gds.ier.model.InprogressCrown
+import scala.Some
+
+class AddressFirstMustacheTest
+  extends FlatSpec
+  with Matchers
+  with AddressFirstForms
+  with AddressFirstMustache
+  with ErrorMessages
+  with FormKeys
+  with TestHelpers {
+
+  self: FormKeys
+    with ErrorMessages
+    with WithSerialiser =>
+
+  val serialiser = jsonSerialiser
+  val addressFirstMustache = new AddressFirstMustache {}
+
+
+  it should "empty progress form should produce empty Model" in {
+    val emptyApplicationForm = addressFirstForm
+
+    val addressFirstModel = addressFirstMustache.transformFormStepToMustacheData(
+      emptyApplicationForm, "url-string-1", Some("url-string-2"))
+
+    addressFirstModel.question.title should be("Do you have a UK address?")
+    addressFirstModel.question.postUrl should be("url-string-1")
+    addressFirstModel.question.backUrl should be("url-string-2")
+
+    addressFirstModel.hasAddressNo.attributes should be("")
+    addressFirstModel.hasAddressYes.attributes should be("")
+
+  }
+
+
+  it should "progress form with valid values should produce Mustache Model with values present" in {
+    val partiallyFilledApplicationForm = addressFirstForm.fill(InprogressCrown(
+      address = Some(LastUkAddress(
+        hasUkAddress = Some(true),
+        address = None
+      ))
+    ))
+
+    val addressFirstModel = addressFirstMustache.transformFormStepToMustacheData(
+      partiallyFilledApplicationForm, "url-string-1", Some("url-string-2"))
+
+    addressFirstModel.question.title should be("Do you have a UK address?")
+    addressFirstModel.question.postUrl should be("url-string-1")
+    addressFirstModel.question.backUrl should be("url-string-2")
+
+    addressFirstModel.hasAddressNo.attributes should be("")
+    addressFirstModel.hasAddressYes.attributes should be("checked=\"checked\"")
+
+  }
+}
