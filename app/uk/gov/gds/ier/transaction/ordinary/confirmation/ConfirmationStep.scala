f2236a4e13c7abe92165f394b2ba2e49cd49494b
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-03-25 17:20:32 +0000

    Post fixes for "back to start link"

--- app/uk/gov/gds/ier/transaction/ordinary/confirmation/ConfirmationStep.scala
+++ app/uk/gov/gds/ier/transaction/ordinary/confirmation/ConfirmationStep.scala
@@ -75,7 +75,7 @@ class ConfirmationStep @Inject ()(val serialiser: JsonSerialiser,
             "postcode" -> validApplication.address.map(_.postcode).getOrElse(""),
             "hasOtherAddress" -> validApplication.otherAddress.map(
               _.otherAddressOption.hasOtherAddress.toString).getOrElse(""),
-            "backToStartUrl" -> RegisterToVoteController.registerToVoteStart.url.toString
+            "backToStartUrl" -> RegisterToVoteController.registerToVote.url.toString
           )
         }
       )
