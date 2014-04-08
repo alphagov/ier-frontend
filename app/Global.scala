64a456abe5d718cb9c6b27cefb09bbedeb380c9d
Antonio Perez <a.perez@kainos.com>
2014-04-08 12:23:24 +0100

    creating filters for cross aspects

--- app/Global.scala
+++ app/Global.scala
@@ -1,11 +1,12 @@
-import play.api.mvc.EssentialAction
 import uk.gov.gds.ier.client._
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.DynamicGlobal
+import uk.gov.gds.ier.filter.{ResultFilter, StatsdFilter}
 import uk.gov.gds.ier.logging.Logging
 import uk.gov.gds.ier.service.apiservice.{ConcreteIerApiService, IerApiService}
 import uk.gov.gds.ier.stubs.{PlacesStubApiClient, IerStubApiClient, IerApiServiceWithStripNino}
-import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc._
+
 
 object Global extends DynamicGlobal with Logging {
 
@@ -29,9 +30,7 @@ object Global extends DynamicGlobal with Logging {
       }
   }
 
-  override def doFilter(action: EssentialAction): EssentialAction = EssentialAction { request =>
-    action.apply(request).map(_.withHeaders(
-      "X-Frame-Options" -> "deny"
-    ))
+  override def doFilter(next: EssentialAction): EssentialAction = {
+    Filters(super.doFilter(next), StatsdFilter, ResultFilter)
   }
 }
