e8c15d1a9e2176213970427a6a211da4b52f932c
michaeldfallen <michael.allen@digital.cabinet-office.gov.uk>
2014-04-09 12:18:44 +0100

    ordinary/dateOfBirth step implemented using the new mustache

--- app/uk/gov/gds/ier/transaction/ordinary/dateOfBirth/DateOfBirthStep.scala
+++ app/uk/gov/gds/ier/transaction/ordinary/dateOfBirth/DateOfBirthStep.scala
@@ -12,13 +12,14 @@ import uk.gov.gds.ier.validation._
 import uk.gov.gds.ier.validation.constants.DateOfBirthConstants
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.security.EncryptionService
-import uk.gov.gds.ier.step.{OrdinaryStep, Routes, GoTo}
+import uk.gov.gds.ier.step.{OrdinaryStepWithNewMustache, Routes, GoTo}
 import uk.gov.gds.ier.transaction.ordinary.InprogressOrdinary
 
-class DateOfBirthStep @Inject ()(val serialiser: JsonSerialiser,
-                                       val config: Config,
-                                       val encryptionService : EncryptionService)
-  extends OrdinaryStep
+class DateOfBirthStep @Inject ()(
+    val serialiser: JsonSerialiser,
+    val config: Config,
+    val encryptionService : EncryptionService
+) extends OrdinaryStepWithNewMustache
   with DateOfBirthForms
   with DateOfBirthMustache{
 
@@ -31,13 +32,6 @@ class DateOfBirthStep @Inject ()(val serialiser: JsonSerialiser,
     editGet = DateOfBirthController.editGet,
     editPost = DateOfBirthController.editPost
   )
-
-  def template(
-      form: ErrorTransformForm[InprogressOrdinary],
-      postEndpoint:Call,
-      backEndpoint: Option[Call]): Html = {
-    dateOfBirthMustache(form, postEndpoint, backEndpoint)
-  }
 
   def nextStep(currentState: InprogressOrdinary) = {
     currentState.dob match {
