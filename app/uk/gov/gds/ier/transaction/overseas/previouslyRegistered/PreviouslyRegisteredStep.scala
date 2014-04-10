abb372fc4e8168db81967ca621e5e309cb038db4
Antonio Perez <a.perez@kainos.com>
2014-04-10 10:45:13 +0100

    overseas previously registered step refactored

--- app/uk/gov/gds/ier/transaction/overseas/previouslyRegistered/PreviouslyRegisteredStep.scala
+++ app/uk/gov/gds/ier/transaction/overseas/previouslyRegistered/PreviouslyRegisteredStep.scala
@@ -4,21 +4,17 @@ import com.google.inject.Inject
 import uk.gov.gds.ier.serialiser.JsonSerialiser
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.security.EncryptionService
-import uk.gov.gds.ier.model.{PreviouslyRegistered}
-import uk.gov.gds.ier.validation.ErrorTransformForm
-import play.api.mvc.Call
-import play.api.templates.Html
+import uk.gov.gds.ier.model.PreviouslyRegistered
 import controllers.step.overseas.routes.{PreviouslyRegisteredController, DateOfBirthController}
 import controllers.step.overseas.{LastRegisteredToVoteController, DateLeftUkController}
-import uk.gov.gds.ier.step.OverseaStep
-import uk.gov.gds.ier.step.Routes
+import uk.gov.gds.ier.step.{OverseaStepWithNewMustache, Routes}
 import uk.gov.gds.ier.transaction.overseas.InprogressOverseas
 
 class PreviouslyRegisteredStep @Inject() (
     val serialiser: JsonSerialiser,
     val config: Config,
     val encryptionService: EncryptionService)
-  extends OverseaStep
+  extends OverseaStepWithNewMustache
   with PreviouslyRegisteredForms
   with PreviousRegisteredMustache {
 
@@ -41,12 +37,5 @@ class PreviouslyRegisteredStep @Inject() (
       }
       case _ => this
     }
-  }
-
-  def template(
-      form: ErrorTransformForm[InprogressOverseas],
-      postEndpoint: Call,
-      backEndpoint:Option[Call]): Html = {
-    previousRegisteredMustache(form, postEndpoint, backEndpoint)
   }
 }
