c009a7de7fd96a6311dfbdfcc5a0394d94e88c78
Chris Duraj <christopherduraj@gmail.com>
2014-04-10 17:25:38 +0100

    New mustache for forces: Nationality step should use new mustache template

--- app/uk/gov/gds/ier/transaction/forces/nationality/NationalityStep.scala
+++ app/uk/gov/gds/ier/transaction/forces/nationality/NationalityStep.scala
@@ -12,7 +12,7 @@ import play.api.templates.Html
 import uk.gov.gds.ier.service.IsoCountryService
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.security.EncryptionService
-import uk.gov.gds.ier.step.{ForcesStep, Routes, GoTo}
+import uk.gov.gds.ier.step.{ForcesStepWithNewMustache, ForcesStep, Routes, GoTo}
 import uk.gov.gds.ier.transaction.forces.InprogressForces
 
 class NationalityStep @Inject ()(
@@ -20,7 +20,7 @@ class NationalityStep @Inject ()(
     val isoCountryService: IsoCountryService,
     val config: Config,
     val encryptionService : EncryptionService)
-  extends ForcesStep
+  extends ForcesStepWithNewMustache
     with NationalityForms
     with NationalityMustache {
 
@@ -33,13 +33,6 @@ class NationalityStep @Inject ()(
     editGet = NationalityController.editGet,
     editPost = NationalityController.editPost
   )
-
-  def template(
-      form: ErrorTransformForm[InprogressForces],
-      postEndpoint: Call,
-      backEndpoint:Option[Call]): Html = {
-    nationalityMustache(form, postEndpoint, backEndpoint)
-  }
 
   def nextStep(currentState: InprogressForces) = {
     if (currentState.nationality.flatMap(_.noNationalityReason) == None) {
