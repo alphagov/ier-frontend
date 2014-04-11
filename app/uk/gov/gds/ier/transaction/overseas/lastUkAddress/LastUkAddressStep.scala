edafd911991509f6a198a891cd2cab58e3df6795
Jianfeng Tian <jianfeng.tian@gmail.com>
2014-04-11 11:06:19 +0100

    add northern ireland exit for lastukaddress step

--- app/uk/gov/gds/ier/transaction/overseas/lastUkAddress/LastUkAddressStep.scala
+++ app/uk/gov/gds/ier/transaction/overseas/lastUkAddress/LastUkAddressStep.scala
@@ -13,7 +13,7 @@ import uk.gov.gds.ier.model.ApplicationType
 import uk.gov.gds.ier.security.EncryptionService
 import uk.gov.gds.ier.serialiser.JsonSerialiser
 import uk.gov.gds.ier.service.AddressService
-import uk.gov.gds.ier.step.{OverseaStep, Routes}
+import uk.gov.gds.ier.step.{OverseaStep, Routes, GoTo}
 import uk.gov.gds.ier.validation.ErrorTransformForm
 import uk.gov.gds.ier.form.OverseasFormImplicits
 import uk.gov.gds.ier.transaction.overseas.InprogressOverseas
@@ -43,10 +43,15 @@ class LastUkAddressStep @Inject() (
   )
 
   def nextStep(currentState: InprogressOverseas) = {
-    currentState.identifyApplication match {
-      case ApplicationType.RenewerVoter => NameController.nameStep
-      case ApplicationType.DontKnow => this
-      case _ => PassportCheckController.passportCheckStep
+    val optAddress = currentState.lastUkAddress 
+    if (optAddress.exists(_.postcode.toUpperCase.startsWith("BT"))) 
+      GoTo (ExitController.northernIreland)
+    else {
+      currentState.identifyApplication match {
+        case ApplicationType.RenewerVoter => NameController.nameStep
+        case ApplicationType.DontKnow => this
+        case _ => PassportCheckController.passportCheckStep
+      }
     }
   }
 
