8f1601c8fc2ee7064c70ec76b85c146e375cf568
Chris Duraj <christopherduraj@gmail.com>
2014-04-11 13:42:59 +0100

    New mustache for forces: Rank step should use new mustache template

--- app/uk/gov/gds/ier/transaction/forces/rank/RankStep.scala
+++ app/uk/gov/gds/ier/transaction/forces/rank/RankStep.scala
@@ -4,19 +4,16 @@ import controllers.step.forces.ContactAddressController
 import controllers.step.forces.routes.{ServiceController, RankController}
 import com.google.inject.Inject
 import uk.gov.gds.ier.serialiser.JsonSerialiser
-import uk.gov.gds.ier.validation._
-import play.api.mvc.Call
-import play.api.templates.Html
 import uk.gov.gds.ier.config.Config
 import uk.gov.gds.ier.security.EncryptionService
-import uk.gov.gds.ier.step.{ForcesStep, Routes}
+import uk.gov.gds.ier.step.{ForcesStepWithNewMustache, Routes}
 import uk.gov.gds.ier.transaction.forces.InprogressForces
 
 class RankStep @Inject ()(
     val serialiser: JsonSerialiser,
     val config: Config,
     val encryptionService : EncryptionService)
-  extends ForcesStep
+  extends ForcesStepWithNewMustache
   with RankForms
   with RankMustache {
 
@@ -29,19 +26,6 @@ class RankStep @Inject ()(
     editGet = RankController.editGet,
     editPost = RankController.editPost
   )
-
-  def template(
-      form: ErrorTransformForm[InprogressForces],
-      call:Call,
-      backUrl: Option[Call]): Html = Html.empty
-
-  override def templateWithApplication(
-      form: ErrorTransformForm[InprogressForces],
-      call: Call,
-      backUrl: Option[Call]):InprogressForces => Html = {
-    application:InprogressForces =>
-      rankMustache(application, form, call, backUrl)
-  }
 
   def nextStep(currentState: InprogressForces) = {
     ContactAddressController.contactAddressStep
