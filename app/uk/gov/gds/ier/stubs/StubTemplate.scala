636524e72dc56e65941669c225e2ab733521938a
michaeldfallen <michael.allen@digital.cabinet-office.gov.uk>
2014-04-15 15:11:23 +0100

    removing unused StubTemplate file

--- app/uk/gov/gds/ier/stubs/StubTemplate.scala
+++
@@ -1,8 +0,0 @@
-package uk.gov.gds.ier.stubs
-
-import uk.gov.gds.ier.step.{StepController, StepTemplate}
-import uk.gov.gds.ier.step.InprogressApplication
-import uk.gov.gds.ier.mustache.MustacheRenderer
-import uk.gov.gds.ier.validation.ErrorTransformForm
-import play.api.mvc.Call
-
