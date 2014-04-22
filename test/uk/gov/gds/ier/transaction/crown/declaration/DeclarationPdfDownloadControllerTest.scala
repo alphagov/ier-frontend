95ba0cfb6b0acb062d5f77b1dd199c80ae790e07
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-04-22 16:16:44 +0100

    Add unit test for PDF download itself
    Add DeclarationPdfDownloadController using streams only and not File and File.toURI facilities suspected of failing on Preview

---
+++ test/uk/gov/gds/ier/transaction/crown/declaration/DeclarationPdfDownloadControllerTest.scala
@@ -0,0 +1,36 @@
+package uk.gov.gds.ier.transaction.crown.declaration
+
+import org.scalatest.{Matchers, FlatSpec}
+import play.api.test.Helpers._
+import scala.Some
+import play.api.test.{FakeRequest, FakeApplication}
+import uk.gov.gds.ier.test.TestHelpers
+import java.nio.file.{Paths, Files}
+import controllers.step.crown.DeclarationPdfDownloadController
+
+class DeclarationPdfDownloadControllerTest extends FlatSpec with Matchers with TestHelpers {
+
+  behavior of "DeclarationPdfDownloadController.download"
+  it should "return binary stream exactly matching PDF file in assets" in {
+    val expectedContent = Files.readAllBytes(
+      Paths.get(
+        DeclarationPdfDownloadController.getClass.getResource(
+          DeclarationPdfDownloadController.pdfFileName).toURI))
+    running(FakeApplication()) {
+      route(
+        FakeRequest(GET, "/register-to-vote/crown/declaration-pdf-download").withIerSession()
+      ) match {
+        case Some(result) => {
+          status(result) should be(OK)
+          contentType(result) should be(Some("application/pdf"))
+          header(CONTENT_LENGTH, result) should be(Some(expectedContent.length.toString))
+          val downloadedContent = contentAsBytes(result)
+          downloadedContent should be(expectedContent)
+          downloadedContent.take(4) should be("%PDF".getBytes)
+          // PDF file headers should be like %PDF−1.0, %PDF−1.1, %PDF−1.2 ...ignore version
+        }
+        case None => fail("Request failed, wrong URL?")
+      }
+    }
+  }
+}
