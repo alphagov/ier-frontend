348431cf308eb9eb24cd6517654f2c4bcc818fad
michaeldfallen <michael.allen@digital.cabinet-office.gov.uk>
2014-05-06 16:04:46 +0100

    session timestamp history storing and tested

--- app/uk/gov/gds/ier/session/SessionToken.scala
+++ app/uk/gov/gds/ier/session/SessionToken.scala
@@ -3,11 +3,13 @@ package uk.gov.gds.ier.session
 import org.joda.time.DateTime
 
 case class SessionToken(
-    timestamp: DateTime = DateTime.now
+    timestamp: DateTime = DateTime.now,
+    history: List[DateTime] = List.empty
 ) {
   def refreshToken() = {
     this.copy(
-      timestamp = DateTime.now
+      timestamp = DateTime.now,
+      history = history :+ timestamp
     )
   }
 }
---
+++ test/uk/gov/gds/ier/session/SessionTokenTests.scala
@@ -0,0 +1,49 @@
+package uk.gov.gds.ier.session
+
+import org.scalatest.{Matchers, FlatSpec}
+import org.joda.time.DateTime
+
+class SessionTokenTests extends FlatSpec with Matchers {
+
+  behavior of "SessionToken.init"
+  it should "init with a new Timestamp" in {
+    val token = SessionToken()
+    token.timestamp.getMillis should equal(DateTime.now.getMillis +- 100)
+  }
+
+  it should "init with an empty history" in {
+    val token = SessionToken()
+    token.history should be(List.empty)
+  }
+
+  behavior of "SessionToken.refreshToken"
+  it should "return a fresh timestamp" in {
+    val token = SessionToken()
+
+    //Do some work to waste cycles
+    1 to 10 map { i => i + 10 }
+
+    val refreshed = token.refreshToken()
+    refreshed.timestamp should not be token.timestamp
+  }
+
+  it should "store the previous timestamp in the history" in {
+    val token = SessionToken()
+    val refreshed = token.refreshToken()
+    refreshed.history.size should be(1)
+    refreshed.history should contain(token.timestamp)
+  }
+
+  it should "maintain a long running history" in {
+    val sessionToken = SessionToken()
+
+    val result = (1 to 100).foldLeft(sessionToken) { case (token, i) =>
+      val refreshed = token.refreshToken()
+      refreshed.history should contain(token.timestamp)
+      refreshed
+    }
+
+    result.history.size should be(100)
+    result.history should contain(sessionToken.timestamp)
+  }
+}
