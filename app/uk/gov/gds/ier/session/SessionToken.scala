9916a4478cb95b567d7b0aed5735b9469f109867
michaeldfallen <michaeldfallen@gmail.com>
2014-05-29 14:01:48 +0100

    prevent the session cookie from growing too large and killing itself

--- app/uk/gov/gds/ier/session/SessionToken.scala
+++ app/uk/gov/gds/ier/session/SessionToken.scala
@@ -6,12 +6,12 @@ case class SessionToken(
     timestamp: DateTime = DateTime.now,
     history: List[DateTime] = List.empty
 ) {
-  require(history.size <= 100)
+  require(history.size <= 50)
 
   def refreshToken() = {
     this.copy(
       timestamp = DateTime.now,
-      history = (timestamp +: history).take(100)
+      history = (timestamp +: history).take(50)
     )
   }
 }
