3e2d5bb58207c4debdce60b144cbd1b4cfb1356f
Tom Byers <tombaromba@gmail.com>
2014-06-04 10:39:31 +0100

    Remove usage of jQuery
    Means it can sit before all other scripts

--- public/javascripts/cache-busting.js
+++ public/javascripts/cache-busting.js
@@ -1,16 +1,24 @@
 // Scripts to ensure requests made to the application from pressing the back button.
+(function () {
+  "use strict";
 
-// 1. Make browsers that ignore no-cache headers (and still use bfcache) to not use bfcache
+  var controls,
+      idx;
 
-if (typeof window.addEventListener !== 'undefined') {
-  window.addEventListener('unload', function(){}, false);
-}
+  // 1. Make browsers that ignore no-cache headers (and still use bfcache) to not use bfcache
+  if (typeof window.addEventListener !== 'undefined') {
+    window.addEventListener('unload', function(){}, false);
+  }
 
-// 2. Force browsers to honour checked="checked" attributes on radios/checkboxes for fresh-page
-//    requests coming from pressing the back button
-
-$('input[type="radio"], input[type="checkbox"]')
-  .filter('[checked]')
-  .each(function () {
-    this.checked = 'checked'; 
-  });
+  // 2. Force browsers to honour checked="checked" attributes on radios/checkboxes for fresh-page
+  //    requests coming from pressing the back button
+  if (typeof document.querySelectorAll !== 'undefined') {
+    controls = document.querySelectorAll('input[type="radio"], input[type="checkbox"]');
+    idx = controls.length;
+    while (idx--) {
+      if (controls[idx].getAttribute('checked') !== null) {
+        controls[idx].checked = 'checked';
+      }
+    }
+  }
+}.call(this));
