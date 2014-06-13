37b73a639bb5282a4779cc928864c3fce472d867
Chris Duraj <christopherduraj@gmail.com>
2014-06-13 13:49:46 +0100

    Ensure previous name is not in API map when hasPreviousName = false

--- app/uk/gov/gds/ier/model/PreviousName.scala
+++ app/uk/gov/gds/ier/model/PreviousName.scala
@@ -3,9 +3,11 @@ package uk.gov.gds.ier.model
 case class PreviousName(hasPreviousName:Boolean,
                         previousName:Option[Name]) {
   def toApiMap(prefix:String = "p"):Map[String,String] = {
-    Map() ++ previousName.map(pn =>
-      pn.toApiMap(prefix + "fn", prefix + "mn", prefix + "ln")
-    ).getOrElse(Map.empty)
+    val previousNameMap =
+      if(hasPreviousName) previousName.map(pn => pn.toApiMap(prefix + "fn", prefix + "mn", prefix + "ln"))
+      else None
+
+    previousNameMap.getOrElse(Map.empty)
   }
 }
 
---
+++ test/uk/gov/gds/ier/model/PreviousNameTests.scala
@@ -0,0 +1,72 @@
+package uk.gov.gds.ier.model
+
+import uk.gov.gds.ier.test.{TestHelpers, CustomMatchers}
+import org.scalatest.{Matchers, FlatSpec}
+
+class PreviousNameTests
+  extends FlatSpec
+  with Matchers
+  with CustomMatchers
+  with TestHelpers {
+
+  behavior of "toApiMap with hasPreviuosName = true"
+  it should "generate the expected payload with first,last and middle names" in {
+    val sutPreviousName = PreviousName(
+      hasPreviousName = true,
+      previousName = Some(Name(
+        firstName = "John",
+        middleNames = Some("James"),
+        lastName = "Smith"
+      ))
+    )
+
+    val expected = Map(
+      "pfn" -> "John",
+      "pmn" -> "James",
+      "pln" -> "Smith"
+    )
+    sutPreviousName.toApiMap() should matchMap(expected)
+  }
+
+  it should "generate the expected payload with first and last names" in {
+    val sutPreviousName = PreviousName(
+      hasPreviousName = true,
+      previousName = Some(Name(
+        firstName = "John",
+        middleNames = None,
+        lastName = "Smith"
+      ))
+    )
+
+    val expected = Map(
+      "pfn" -> "John",
+      "pln" -> "Smith"
+    )
+    sutPreviousName.toApiMap() should matchMap(expected)
+  }
+
+
+  behavior of "toApiMap with hasPreviuosName = false"
+  it should "generate the expected empty map" in {
+    val sutPreviousName = PreviousName(
+      hasPreviousName = false,
+      previousName = None
+    )
+
+    sutPreviousName.toApiMap() shouldBe empty
+  }
+
+  it should "generate the expected empty map even if name and last name are not empty" in {
+    val sutPreviousName = PreviousName(
+      hasPreviousName = false,
+      previousName = Some(Name(
+        firstName = "John",
+        middleNames = None,
+        lastName = "Smith"
+      ))
+    )
+
+    sutPreviousName.toApiMap() shouldBe empty
+  }
+
+}
