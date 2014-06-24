9663effbf2d7814faed55f62a6de9b528eaf15c8
januhlir <jan.uhlir@digital.cabinet-office.gov.uk>
2014-06-24 23:31:40 +0100

    Just formatting

--- app/uk/gov/gds/ier/session/CustomizedSessionHandling.scala
+++ app/uk/gov/gds/ier/session/CustomizedSessionHandling.scala
@@ -25,48 +25,45 @@ abstract class CustomizedSessionHandling[T](payloadCookieKey: String)
 
   object ValidSession {
     final def validateSession[A](
-      bodyParser: BodyParser[A],
-      block:Request[A] => T => Result
-    )(
-      implicit manifest: Manifest[T]
-    ): Action[A] = Action(bodyParser) {
-      implicit request =>
-        logger.debug(s"REQUEST ${request.method} ${request.path} - Valid Session needed")
-        request.getToken match {
-          case Some(token) => {
-            token.isValid match {
-              case true => {
-                val application = request.getPayload.getOrElse(factoryOfT())
-                logger.debug(s"Validate session - token is valid")
-                val result = block(request)(application)
-                result storeToken token.refreshToken
-              }
-              case false => {
-                logger.debug(s"Validate session - token is not valid ${serialiser.toJson(token)}")
-                Redirect(routes.ErrorController.timeout()).withFreshSession()
+        bodyParser: BodyParser[A],
+        block:Request[A] => T => Result)(
+        implicit manifest: Manifest[T]): Action[A] =
+      Action(bodyParser) {
+        implicit request =>
+          logger.debug(s"REQUEST ${request.method} ${request.path} - Valid Session needed")
+          request.getToken match {
+            case Some(token) => {
+              token.isValid match {
+                case true => {
+                  val application = request.getPayload.getOrElse(factoryOfT())
+                  logger.debug(s"Validate session - token is valid")
+                  val result = block(request)(application)
+                  result storeToken token.refreshToken
+                }
+                case false => {
+                  logger.debug(s"Validate session - token is not valid ${serialiser.toJson(token)}")
+                  Redirect(routes.ErrorController.timeout()).withFreshSession()
+                }
               }
             }
-          }
-          case None => {
-            logger.debug(s"Validate session - Request has no token, refreshing and redirecting to govuk start page")
-            Redirect(config.ordinaryStartUrl).withFreshSession()
+            case None => {
+              logger.debug(s"Validate session - Request has no token, refreshing and redirecting to govuk start page")
+              Redirect(config.ordinaryStartUrl).withFreshSession()
+            }
           }
       }
-    }
 
     final def withParser[A](
-        bodyParser: BodyParser[A]
-      )(
-        implicit manifest: Manifest[T]
-      ) = new {
-        def requiredFor(action: Request[A] => T => Result) = validateSession(bodyParser, action)
-      }
+        bodyParser: BodyParser[A])(
+        implicit manifest: Manifest[T]) = new {
+      def requiredFor(action: Request[A] => T => Result) = validateSession(bodyParser, action)
+    }
 
     final def requiredFor(
-        action: Request[AnyContent] => T => Result
-      )(
-        implicit manifest: Manifest[T]
-      ) = withParser(BodyParsers.parse.anyContent) requiredFor action
+        action: Request[AnyContent] => T => Result)(
+        implicit manifest: Manifest[T]) = {
+      withParser(BodyParsers.parse.anyContent) requiredFor action
+    }
   }
 
   implicit class SessionCookieExtractor(request: play.api.mvc.Request[_]) {
