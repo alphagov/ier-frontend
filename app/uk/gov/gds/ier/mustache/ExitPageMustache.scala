93f86c0170aa030b35e18e03d77c5ea9fe8fa5c0
michaeldfallen <michaeldfallen@gmail.com>
2014-07-01 11:02:18 +0100

    switched the exit pages to inherited templates

--- app/uk/gov/gds/ier/mustache/ExitPageMustache.scala
+++ app/uk/gov/gds/ier/mustache/ExitPageMustache.scala
@@ -3,83 +3,44 @@ package uk.gov.gds.ier.mustache
 import uk.gov.gds.ier.guice.{WithRemoteAssets, WithConfig}
 import uk.gov.gds.ier.langs.Messages
 
-trait ExitPageMustache extends StepMustache {
+trait ExitPageMustache extends InheritedGovukMustache {
   self: WithRemoteAssets
     with WithConfig =>
 
   object ExitPages {
 
-    case class BritishIslands(
-        title: Option[String] = None
-    ) (
-        implicit val lang: Lang
-    ) extends ArticlePage("exit/britishIslands")
-      with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_britishIslands_title")
-    }
+    case class BritishIslands() (
+        implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/britishIslands")
 
-    case class DontKnow(
-      title: Option[String] = None
-    ) (
-      implicit val lang: Lang
-    ) extends ArticlePage("exit/dontKnow")
-    with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_dontKnow_title")
-    }
+    case class DontKnow() (
+        implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/dontKnow")
 
-    case class NoFranchise(
-      title: Option[String] = None
-    ) (
-      implicit val lang: Lang
-    ) extends ArticlePage("exit/noFranchise")
-      with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_nationality_noFranchise_title")
-    }
+    case class NoFranchise() (
+      implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/noFranchise")
 
-    case class NorthernIreland(
-        title: Option[String] = None
-    ) (
-        implicit val lang: Lang
-    ) extends ArticlePage("exit/northernIreland")
-      with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_northernIreland_title")
-    }
+    case class NorthernIreland() (
+        implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/northernIreland")
 
-    case class Scotland (
-      implicit val lang: Lang
-    ) extends ArticlePage("exit/scotland")
-      with MessagesForMustache {
-      val pageTitle =  Messages("exit_scotland_title")
-    }
+    case class Scotland() (
+      implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/scotland")
 
-    case class TooYoung (
-        title: Option[String] = None
-    ) (
-        implicit val lang: Lang
-    ) extends ArticlePage("exit/tooYoung")
-      with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_tooYoung_title")
-    }
+    case class TooYoung () (
+        implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/tooYoung")
 
-    case class Under18 (
-        title: Option[String] = None
-    ) (
-        implicit val lang: Lang
-    ) extends ArticlePage("exit/under18")
-      with MessagesForMustache {
-      val pageTitle = title getOrElse Messages("exit_under18_title")
-    }
+    case class Under18 () (
+        implicit override val lang: Lang
+    ) extends ArticleMustachio("exit/under18")
 
-    case class LeftService (
-        pageTitle: String = "Sorry, you cannot registers because it has been over 15 years since you left the position"
-    ) extends ArticlePage("exit/leftService")
+    case class LeftService () extends ArticleMustachio("exit/leftService")
 
-    case class LeftUk (
-        pageTitle: String = "Sorry, you cannot registers because it has been over 15 years since you left the UK"
-    ) extends ArticlePage("exit/leftUk")
+    case class LeftUk () extends ArticleMustachio("exit/leftUk")
 
-    case class TooOldWhenLeft (
-        pageTitle: String = "Sorry, you cannot register to vote overseas because you were not registered to vote when you lived in the UK"
-    ) extends ArticlePage("exit/tooOldWhenLeft")
+    case class TooOldWhenLeft () extends ArticleMustachio("exit/tooOldWhenLeft")
   }
 }
