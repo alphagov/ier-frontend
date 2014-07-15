4614ad78c5158f98ff2639e89ef00c16114f4e32
Chris Duraj <christopherduraj@gmail.com>
2014-07-15 15:52:59 +0100

    Add JS validation to postcode lookup

--- app/uk/gov/gds/ier/localAuthority/LocalAuthorityLookupForm.scala
+++ app/uk/gov/gds/ier/localAuthority/LocalAuthorityLookupForm.scala
@@ -2,18 +2,28 @@ package uk.gov.gds.ier.localAuthority
 
 import uk.gov.gds.ier.validation.FormKeys
 import uk.gov.gds.ier.validation.ErrorTransformForm
-import play.api.data._
 import play.api.data.Forms._
 import uk.gov.gds.ier.validation.PostcodeValidator
+import play.api.data.validation.{Invalid, Valid, Constraint}
 
 trait LocalAuthorityLookupForm {
   self: FormKeys =>
 
   lazy val localAuthorityLookupForm = ErrorTransformForm(
     mapping(
-      keys.postcode.key -> nonEmptyText.verifying(PostcodeValidator.isValid(_))
+      keys.postcode.key -> nonEmptyText
     )
     (LocalAuthorityRequest.apply)(LocalAuthorityRequest.unapply)
+    .verifying(isPostcodeValid)
   )
+
+  lazy val isPostcodeValid = Constraint[LocalAuthorityRequest](keys.postcode.key) {
+    localAuthorityRequest =>
+    PostcodeValidator.isValid(localAuthorityRequest.postcode) match {
+      case true => Valid
+      case _ => Invalid("ordinary_address_error_postcodeIsNotValid", keys.postcode)
+    }
+  }
+
 }
 
