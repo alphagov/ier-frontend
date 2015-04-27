bbc4f1682391a5171051a1df4dbf8a89718bc74a
Sheree Atcheson <s.atcheson@kainos.com>
2015-04-27 19:24:33 +0100

    validation now working as expected.

--- app/uk/gov/gds/ier/validation/PassportNumberValidator.scala
+++ app/uk/gov/gds/ier/validation/PassportNumberValidator.scala
@@ -2,7 +2,7 @@ package uk.gov.gds.ier.validation
 
 object PassportNumberValidator {
 
-  val passportNumberRegex = "^[0-9]{9}$/"
+  val passportNumberRegex = "[0-9]{9}"
 
   def isValid(passportNumber: String) = {
     passportNumber.replaceAll("\\s", "").matches(passportNumberRegex)
