93b258cc75fd5fc7cb2f0958f1711332f38e6f5f
Stephen McCalden <S.McCalden@kainos.com>
2015-05-18 15:51:43 +0100

    IER0191 : Added new CountryValidator with isScotland Boolean response based on the country of residence chosen

--- app/uk/gov/gds/ier/validation/CountryValidator.scala
+++ app/uk/gov/gds/ier/validation/CountryValidator.scala
@@ -1,12 +1,13 @@
 package uk.gov.gds.ier.validation
 
-/**
- * Created with IntelliJ IDEA.
- * User: stephenca
- * Date: 18/05/15
- * Time: 14:36
- * To change this template use File | Settings | File Templates.
- */
-class CountryValidator {
+import uk.gov.gds.ier.model.Country
 
+object CountryValidator {
+
+  def isScotland(country: Option[Country]):Boolean = {
+    country match {
+      case Some(Country("Scotland", _)) => true
+      case _ => false
+    }
+  }
 }
