7db7ac6afcd9f15568935bac5c2e58cb212ef005
Simon Moore <S.Moore@kainos.com>
2015-08-06 17:17:15 +0100

    Reduce minimum phone number validation length from 7 digits to 3 digits

--- app/uk/gov/gds/ier/validation/TelephoneValidator.scala
+++ app/uk/gov/gds/ier/validation/TelephoneValidator.scala
@@ -2,7 +2,7 @@ package uk.gov.gds.ier.validation
 
 object TelephoneValidator {
 
-  val telephoneRegex = "^[0-9]{7,30}$"
+  val telephoneRegex = "^[0-9]{3,30}$"
 
   def isValid(telephone: String) = {
     telephone.replaceAll("[\\s\\+\\-\\_\\(\\)\\[A-Za-z]", "").matches(telephoneRegex)
