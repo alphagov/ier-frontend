d8cef0e299d7d3e216fe66ff51ff700688681cf6
Simon Moore <S.Moore@kainos.com>
2016-03-16 13:24:27 +0000

    Set the email language based on the domain of the sessionKey cookie instead of the html language of the page.

--- app/uk/gov/gds/ier/langs/Language.scala
+++ app/uk/gov/gds/ier/langs/Language.scala
@@ -6,11 +6,19 @@ object Language {
   val Lang = play.api.i18n.Lang
   type Lang = play.api.i18n.Lang
   def english = Lang("en-GB")
+  def welsh = Lang("cy")
   var emailLang = ""
 
   def getLang(request:Request[Any]):Lang = {
     request.acceptLanguages.headOption.getOrElse(english)
   }
 
-  def setLang = (str: String) => emailLang = str
+  def setEmailLang(request:Request[Any]){
+    if ((request.cookies.get("sessionKey").get.domain.getOrElse("") contains "cymraeg") || (request.cookies.get("sessionKey").get.domain.getOrElse("") contains "welsh")) {
+      emailLang="cy"
+    }
+    else {
+      emailLang="en"
+    }
+  }
 }
