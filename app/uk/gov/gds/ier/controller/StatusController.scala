d34b17e36802bfdec59aded8ff4305d0d6179359
simonmoore100 <s.moore@kainos.com>
2016-09-14 09:16:37 +0100

    Create healthcheck for mongo and postgres and report to /status page

--- app/uk/gov/gds/ier/controller/StatusController.scala
+++ app/uk/gov/gds/ier/controller/StatusController.scala
@@ -1,20 +1,26 @@
 package uk.gov.gds.ier.controller
 
 import play.api.mvc.{Action, Controller}
+
 import sys.process._
 import com.google.inject.Inject
-import uk.gov.gds.ier.serialiser.{WithSerialiser, JsonSerialiser}
+import uk.gov.gds.ier.serialiser.{JsonSerialiser, WithSerialiser}
 import uk.gov.gds.ier.config.Config
 import java.lang.management.ManagementFactory._
 import scala.Some
-import uk.gov.gds.ier.client.ApiResults
+import uk.gov.gds.ier.client.{ApiResults, IerApiClient}
 import com.google.inject.Singleton
+import uk.gov.gds.ier.model.{Fail, LocalAuthority, Success}
+import uk.gov.gds.ier.service.{ApiException, LocateService}
+import uk.gov.gds.ier.service.apiservice.IerApiService
 
 @Singleton
 class StatusController @Inject() (
-    val serialiser: JsonSerialiser,
-    config: Config
-  ) extends Controller with ApiResults with WithSerialiser {
+                                   val serialiser: JsonSerialiser,
+                                   config: Config,
+                                   locateService: LocateService,
+                                   apiClient: IerApiClient
+                                 ) extends Controller with ApiResults with WithSerialiser {
 
   def status = Action {
     okResult(Map(
@@ -25,11 +31,15 @@ class StatusController @Inject() (
       "build date" -> config.buildDate,
       "build number" -> config.buildNumber,
       "revision" -> config.revision,
-      "branch" -> config.branch
+      "branch" -> config.branch,
+      "mongo" -> testMongo,
+      "postgres" -> testPostgres
     ))
   }
 
   private def pid = getRuntimeMXBean.getName.split('@').headOption
+  private def address = locateService.lookupAddress("x11xx")
+  private def localAuthority = getLocalAuthorityByGssCode("E99999999").toString
 
   private def pidAsString = pid match {
     case Some(p) => p
@@ -44,5 +54,27 @@ class StatusController @Inject() (
   private def startTime = pid match {
     case Some(p) => "ps -o lstart= %s".format(p).!!.trim
     case None => "Uptime Unavailable"
+  }
+
+  private def testMongo = address.mkString(",") match {
+    case address if address.contains("property") => "up"
+    case _ => "Mongo Unavailable"
+  }
+
+  private def testPostgres = localAuthority match {
+    case localAuthority if localAuthority.contains("E99999999") => "up"
+    case _ => "Postgres Unavailable"
+  }
+
+  def getLocalAuthorityByGssCode(gssCode: String): LocalAuthority = {
+    apiClient.get(config.ierLocalAuthorityLookupUrl + gssCode,
+      ("Authorization", "BEARER " + config.ierApiToken)) match {
+      case Success(body,timeTakenMs) => {
+        serialiser.fromJson[LocalAuthority](body)
+      }
+      case Fail(error,timeTakenMs) => {
+        throw new ApiException(error)
+      }
+    }
   }
 }
